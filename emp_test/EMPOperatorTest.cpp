5fb3e9ac881e1b3b51dccaa5ed9ef84cb0a938ba
Li Li (Ads) <lil@fb.com>
2021-02-26 01:57:09 -0800

    add unittest for intXor and bitXor
    Summary: As we are using Xor operation in GroupedEncryptedLiftMetrics and GroupedLiftMetrics, this diff adds unittests for intXor and bitXor
    
    Reviewed By: peking2
    
    Differential Revision: D26675906
    
    fbshipit-source-id: fd2e91293288ec7b0e9d0181ea6adecb4f737a71

--- emp_test/EMPOperatorTest.cpp
+++ emp_test/EMPOperatorTest.cpp
@@ -35,8 +35,14 @@ class EMPOperatorTestFixture : public ::testing::Test {
   std::function<emp::Integer(emp::Integer, emp::Integer)> intMultiply =
       [](const emp::Integer& x, const emp::Integer& y) { return x * y; };
 
+  std::function<emp::Integer(emp::Integer, emp::Integer)> intXor =
+      [](const emp::Integer& x, const emp::Integer& y) { return x ^ y; };
+
   std::function<emp::Bit(emp::Bit, emp::Bit)> bitAnd =
       [](const emp::Bit& x, const emp::Bit& y) { return x & y; };
+
+  std::function<emp::Bit(emp::Bit, emp::Bit)> bitXor =
+      [](const emp::Bit& x, const emp::Bit& y) { return x ^ y; };
 };
 
 TEST_F(EMPOperatorTestFixture, IntAdditionTwoPositives) {
@@ -87,6 +93,24 @@ TEST_F(EMPOperatorTestFixture, IntAdditionRandomNumbers) {
   EXPECT_EQ(ans_expected, res2);
 }
 
+TEST_F(EMPOperatorTestFixture, IntXorRandomNumbers) {
+  int64_t a = folly::Random::rand32();
+  int64_t b = folly::Random::rand32();
+  int64_t ans_expected = a ^ b;
+  EMPOperatorTestConfig<int64_t, emp::Integer> aliceConfig{intXor, a};
+  EMPOperatorTestConfig<int64_t, emp::Integer> bobConfig{intXor, b};
+
+  auto [res1, res2] = pcf::mpc::test<
+      EMPOperator<
+          int64_t,
+          emp::Integer,
+          EMPOperatorTestConfig<int64_t, emp::Integer>>,
+      EMPOperatorTestConfig<int64_t, emp::Integer>,
+      int64_t>(aliceConfig, bobConfig);
+  EXPECT_EQ(ans_expected, res1);
+  EXPECT_EQ(ans_expected, res2);
+}
+
 TEST_F(EMPOperatorTestFixture, IntMultiplicationRandomNumbers) {
   int64_t a = folly::Random::rand32(3037000500);
   int64_t b = folly::Random::rand32(3037000500);
@@ -111,6 +135,21 @@ TEST_F(EMPOperatorTestFixture, BitAnd) {
   bool ans_expected = static_cast<bool>((a ? 1 : 0) & (b ? 1 : 0));
   EMPOperatorTestConfig<bool, emp::Bit> aliceConfig{bitAnd, a};
   EMPOperatorTestConfig<bool, emp::Bit> bobConfig{bitAnd, b};
+
+  auto [res1, res2] = pcf::mpc::test<
+      EMPOperator<bool, emp::Bit, EMPOperatorTestConfig<bool, emp::Bit>>,
+      EMPOperatorTestConfig<bool, emp::Bit>,
+      bool>(aliceConfig, bobConfig);
+  EXPECT_EQ(ans_expected, res1);
+  EXPECT_EQ(ans_expected, res2);
+}
+
+TEST_F(EMPOperatorTestFixture, BitXor) {
+  bool a = true;
+  bool b = false;
+  bool ans_expected = a ^ b;
+  EMPOperatorTestConfig<bool, emp::Bit> aliceConfig{bitXor, a};
+  EMPOperatorTestConfig<bool, emp::Bit> bobConfig{bitXor, b};
 
   auto [res1, res2] = pcf::mpc::test<
       EMPOperator<bool, emp::Bit, EMPOperatorTestConfig<bool, emp::Bit>>,
