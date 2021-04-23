3e1b9edc806e352439db5494ba249f11b46045b4
Kyle Johnson <kej@fb.com>
2021-04-23 07:39:10 -0700

    CentOS Docker: Enable building docker images in ubuntu or centos (new default)
    Summary:
    Enable building docker images in ubuntu or centos (new default) by passing in `-u` for ubuntu or `-c` for centos (or nothing will default to centos)
    
    NOTE: Centos docker files do not exist yet (coming in future diffs)
    
    Reviewed By: peking2
    
    Differential Revision: D27883951
    
    fbshipit-source-id: a4f09ef9650986c40e4450a581f68c74a6a7849f

--- build-docker.sh
+++ build-docker.sh
@@ -4,41 +4,67 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+set -e
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-# Docker build must run from this script build dir, so all the relative paths work inside the Dockerfile's
-cd "$SCRIPT_DIR" || exit
-
+CENTOS_RELEASE="8.3.2011"
 UBUNTU_RELEASE="20.04"
 EMP_RELEASE="0.1"
 AWS_RELEASE="1.8.177"
 FMT_RELEASE="7.1.3"
 FOLLY_RELEASE="2021.03.29.00"
 
+PROG_NAME=$0
+usage() {
+  cat << EOF >&2
+Usage: $PROG_NAME [-u | --ubuntu]
+
+-c: builds the docker images aginsts centos (default)
+-u: builds the docker images against ubuntu
+EOF
+  exit 1
+}
+
+IMAGE_PREFIX="centos"
+OS_RELEASE=${CENTOS_RELEASE}
+DOCKER_EXTENSION=""
+while getopts u o; do
+  case $o in
+    (u) IMAGE_PREFIX="ubuntu"
+        OS_RELEASE=${UBUNTU_RELEASE}
+        DOCKER_EXTENSION=".ubuntu";;
+    (*) usage
+  esac
+done
+shift "$((OPTIND - 1))"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# Docker build must run from this script build dir, so all the relative paths work inside the Dockerfile's
+cd "$SCRIPT_DIR" || exit
+
 printf "\nBuilding emp %s docker image...\n" ${EMP_RELEASE}
 docker build \
-    --build-arg ubuntu_release=${UBUNTU_RELEASE} \
+    --build-arg os_release=${OS_RELEASE} \
     --build-arg emp_release=${EMP_RELEASE} \
-    -t fbpcf/ubuntu-emp:${EMP_RELEASE} -f docker/emp/. .
+    -t fbpcf/${IMAGE_PREFIX}-emp:${EMP_RELEASE} -f docker/emp/Dockerfile${DOCKER_EXTENSION} .
 
 printf "\nBuilding aws(s3/core) %s docker image...\n" ${AWS_RELEASE}
 docker build  \
-    --build-arg ubuntu_release=${UBUNTU_RELEASE} \
+    --build-arg os_release=${OS_RELEASE} \
     --build-arg aws_release=${AWS_RELEASE} \
-    -t fbpcf/ubuntu-aws-s3-core:${AWS_RELEASE} -f docker/aws-s3-core/. .
+    -t fbpcf/${IMAGE_PREFIX}-aws-s3-core:${AWS_RELEASE} -f docker/aws-s3-core/Dockerfile${DOCKER_EXTENSION} .
 
 printf "\nBuilding folly %s docker image...\n" ${FOLLY_RELEASE}
 docker build  \
-    --build-arg ubuntu_release=${UBUNTU_RELEASE} \
+    --build-arg os_release=${OS_RELEASE} \
     --build-arg folly_release=${FOLLY_RELEASE} \
     --build-arg fmt_release=${FMT_RELEASE} \
-    -t fbpcf/ubuntu-folly:${FOLLY_RELEASE} -f docker/folly/. .
+    -t fbpcf/${IMAGE_PREFIX}-folly:${FOLLY_RELEASE} -f docker/folly/Dockerfile${DOCKER_EXTENSION} .
 
 printf "\nBuilding fbpcf docker image...\n"
 docker build  \
-    --build-arg ubuntu_release=${UBUNTU_RELEASE} \
+    --build-arg os_release=${OS_RELEASE} \
     --compress \
-    -t fbpcf:latest -f docker/. .
+    -t fbpcf:latest -f docker/Dockerfile${DOCKER_EXTENSION} .
 # FYI: To create a "dev" build (with all source), comment out the docker build above
 #     and uncommment the lines below
 # docker build  \
