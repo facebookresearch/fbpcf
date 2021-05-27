e07d6b2ac05756193c9978971280384d755d2dc3
Ajay Baaskaran <ajaybh@fb.com>
2021-05-26 23:37:14 -0700

    Added sanity check for fbpcf docker image
    Summary:
    Added a sanity check script that runs `millionaire` binary to sanity check if fbpcf docker image built successfully.
    Next step is to automate this using GitHub actions.
    
    Reviewed By: peking2
    
    Differential Revision: D28735137
    
    fbshipit-source-id: d0a479266d0907c80e96ecfa4b47e5d4a38e346f

---
+++ run-millionaire-sample.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -e
+
+PROG_NAME=$0
+usage() {
+  cat << EOF >&2
+Usage: $PROG_NAME [-u | -c]
+
+-c: runs the millionaire sample using the centos docker image
+-u: runs the millionaire sample using the ubuntu docker image (default)
+EOF
+  exit 1
+}
+
+IMAGE_PREFIX="ubuntu"
+while getopts u,c o; do
+  case $o in
+    (u) IMAGE_PREFIX="ubuntu";;
+    (c) IMAGE_PREFIX="centos";;
+    (*) usage
+  esac
+done
+shift "$((OPTIND - 1))"
+
+FBPCF_IMAGE=fbpcf/${IMAGE_PREFIX}:latest
+if docker image inspect ${FBPCF_IMAGE} > /dev/null 2>&1; then
+  printf "Found %s docker image..." ${FBPCF_IMAGE}
+else
+  printf "\nERROR: Unable to find docker image %s. Please run build-docker.sh " ${FBPCF_IMAGE}
+  exit 1
+fi
+
+docker run --rm \
+    --network=host ${FBPCF_IMAGE} \
+        millionaire \
+        --role=1 \
+        --port=5000 \
+        2>&1 publisher & # Fork to background so "partner" can run below
+
+if docker run --rm \
+    --network=host ${FBPCF_IMAGE} \
+        millionaire \
+        --role=2 \
+        --port=5000 \
+        --server_ip=127.0.0.1 | grep -q "connected"; then
+  printf "Millionaire ran successfully! "
+else
+  printf "Something went wrong. You may want to check the logs above."
+  exit 1
+fi
