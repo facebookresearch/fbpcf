d2cac91d13d9b62272162982f773171be3ee2bb6
Tal Davidi <tdavidi@fb.com>
2022-03-21 10:10:06 -0700

    2P Tup Gen + BiDirectional OT Notes
    Summary: Added some notes around the 2P tuple generator as well as the rcot based bidirectional OT used in the product share generator. I wrote these while working on the composite tuple generator design and thought it would be helpful for anyone looking into it later.
    
    Reviewed By: elliottlawrence
    
    Differential Revision: D34807937
    
    fbshipit-source-id: bb82a1375e62cf9932fa0ea744e86379a00e38e0

--- fbpcf/engine/tuple_generator/oblivious_transfer/RcotBasedBidirectionObliviousTransfer_impl.h
+++ fbpcf/engine/tuple_generator/oblivious_transfer/RcotBasedBidirectionObliviousTransfer_impl.h
@@ -29,10 +29,15 @@ RcotBasedBidirectionObliviousTransfer<T>::RcotBasedBidirectionObliviousTransfer(
 
 /**
  * From rcot to ot:
- * assume the sender gets random x0, x1 from rcot and receiver gets b and xb
- * Then the the two parties are secret sharing (x1 - x0)*b:
- * sender's share is -x0 and receiver's share is xb
- * To convert this into a OT with chosen inputs and choice
+ * assume the sender gets random k0, k1 from rcot and receiver gets p and kp
+ * Then the the two parties are secret sharing (k1 - k0)*p = delta * p:
+ * sender's share is -k0 and receiver's share is kp
+ * To convert this into a OT with chosen inputs and choice bit, each party will
+ * send their choice masked by the rcot bit to the other party. The other party
+ * will then compute two corrections based on this masked bit + the messages
+ * they want to send + rcot messages. The parties will exchange these correction
+ * messages and retrieve the chosen message using their choice bit and rcot
+ * message.
  */
 template <class T>
 std::vector<T> RcotBasedBidirectionObliviousTransfer<T>::biDirectionOT(
@@ -44,10 +49,10 @@ std::vector<T> RcotBasedBidirectionObliviousTransfer<T>::biDirectionOT(
   auto future =
       std::async([otSize, this]() { return receiverRcot_->rcot(otSize); });
 
-  // u_0
+  // k0 / l0
   auto rcotSender0Messages = senderRcot_->rcot(otSize);
 
-  // u_c
+  // lr / kp
   auto rcotReceiverMessages = future.get();
 
   assert(rcotSender0Messages.size() == otSize);
@@ -55,34 +60,46 @@ std::vector<T> RcotBasedBidirectionObliviousTransfer<T>::biDirectionOT(
 
   std::vector<__m128i> rcotSender1Messages(otSize);
   for (size_t i = 0; i < otSize; i++) {
-    // u_1
+    // k1 / l1
     rcotSender1Messages[i] = _mm_xor_si128(rcotSender0Messages[i], delta_);
   }
 
   std::vector<bool> maskedChoice(otSize);
   for (size_t i = 0; i < otSize; i++) {
-    // c ^ b
+    // r + choice1 / p + choice2
     maskedChoice[i] = util::getLsb(rcotReceiverMessages[i]) ^ choice[i];
   }
 
   agent_->sendBool(maskedChoice);
-  // c ^ b
+  // p + choice2 / r + choice1
   auto flipIndicator = agent_->receiveBool(otSize);
 
   assert(flipIndicator.size() == otSize);
-
+  // h(k0) / h(l0)
   hashFromAes_.inPlaceHash(rcotSender0Messages);
+  // h(k1) / h(l1)
   hashFromAes_.inPlaceHash(rcotSender1Messages);
+  // h(lr) / h(kp)
   hashFromAes_.inPlaceHash(rcotReceiverMessages);
 
   std::vector<T> maskedInput0(otSize);
   std::vector<T> maskedInput1(otSize);
 
   for (size_t i = 0; i < otSize; i++) {
+    // h(key, n) = key & (1 << |x| - 1) if n >=128
+    // h(key, n) = AES_PRG(key, n) if n > 128
+    // mask(x, key) = x + h(key, |x|)
+
+    // mask(x0, p + choice2 ? k1 : k0) / mask(y0, r + choice1 ? l1 ? l0)
+    // x0 + h((p + choice2) * delta1 + k0, |x0|) /
+    // y0 + h((r + choice1) * delta2 + l0, |y0|)
     maskedInput0[i] = util::Masker<T>::mask(
         input0[i],
         flipIndicator[i] ? rcotSender1Messages[i] : rcotSender0Messages[i]);
 
+    // mask(x1, p + choice2 ? k0 : k1) / mask(y1, r + choice1 ? l0 ? l1)
+    // x1 + h((p + choice2) * delta1 + k1, |x1|) /
+    // y1 + h((r + choice1) * delta2 + l1, |y1|)
     maskedInput1[i] = util::Masker<T>::mask(
         input1[i],
         flipIndicator[i] ? rcotSender0Messages[i] : rcotSender1Messages[i]);
@@ -90,7 +107,11 @@ std::vector<T> RcotBasedBidirectionObliviousTransfer<T>::biDirectionOT(
 
   agent_->sendT<T>(maskedInput0);
   agent_->sendT<T>(maskedInput1);
+  // c0 = y0 + h((r + choice1) * delta2 + l0, |y0|) /
+  // c0 = x0 + h((p + choice2) * delta1 + k0, |x0|)
   auto correction0 = agent_->receiveT<T>(maskedInput0.size());
+  // c1 = y1 + h((r + choice1) * delta2 + l1, |y1|) /
+  // c1 = x1 + h((p + choice2) * delta1 + k1, |x1|)
   auto correction1 = agent_->receiveT<T>(maskedInput1.size());
 
   assert(correction0.size() == otSize);
@@ -99,6 +120,24 @@ std::vector<T> RcotBasedBidirectionObliviousTransfer<T>::biDirectionOT(
   std::vector<T> output(otSize);
 
   for (size_t i = 0; i < otSize; i++) {
+    // unmask(key, b, c0, c1) = h(key, |c0|) + (b ? c1 : c0)
+
+    // unmask(lr, choice1, c0, c1) =
+    // h(lr, |c0|) +
+    // choice1 ?
+    //  y1 + h((r + choice1) * delta2 + l1, |y1|) :
+    //  y0 + h((r + choice1) * delta2 + l0, |y0|)
+    //
+    // = h(lr, |c0|) + choice1 ?
+    //  y1 + h((r + 1) * delta2 + l1, |y1|)
+    //= y1 + h(r * delta2 + delta2 + l1, |y1|)
+    //= y1 + h(r * delta2 + l0, |x1|)
+    //= y1 + h(lr, |x1|) :
+    //  y0 + h((r + 0) * delta2 + l0, |x0|)
+    //= y0 + h(lr, |x0|)
+    // = choice1 ? y1 : y0 /
+    //
+    // = choice2 ? x1 : x0
     output[i] = util::Masker<T>::unmask(
         rcotReceiverMessages[i], choice[i], correction0[i], correction1[i]);
   }
