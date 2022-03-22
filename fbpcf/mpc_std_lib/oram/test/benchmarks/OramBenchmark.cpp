a237919e0a4938920bb3b9edcc6c68d5406aeefb
Elliott Lawrence <elliottlawrence@fb.com>
2022-03-22 12:17:23 -0700

    single point array generator
    Differential Revision: https://www.internalfb.com/diff/D34935731?entry_point=27
    
    fbshipit-source-id: 69aa1bcdb14eb71ec72233bf4f1e71f312b87504

--- fbpcf/mpc_std_lib/oram/test/benchmarks/OramBenchmark.cpp
+++ fbpcf/mpc_std_lib/oram/test/benchmarks/OramBenchmark.cpp
@@ -13,7 +13,9 @@
 #include "fbpcf/engine/util/test/benchmarks/NetworkedBenchmark.h"
 #include "fbpcf/mpc_std_lib/oram/DifferenceCalculatorFactory.h"
 #include "fbpcf/mpc_std_lib/oram/IDifferenceCalculatorFactory.h"
+#include "fbpcf/mpc_std_lib/oram/ISinglePointArrayGenerator.h"
 #include "fbpcf/mpc_std_lib/oram/ObliviousDeltaCalculatorFactory.h"
+#include "fbpcf/mpc_std_lib/oram/SinglePointArrayGeneratorFactory.h"
 #include "fbpcf/mpc_std_lib/util/test/util.h"
 #include "fbpcf/scheduler/IScheduler.h"
 #include "fbpcf/scheduler/SchedulerHelper.h"
@@ -152,6 +154,83 @@ class ObliviousDeltaCalculatorBenchmark
 
 BENCHMARK_COUNTERS(ObliviousDeltaCalculator_Benchmark, counters) {
   ObliviousDeltaCalculatorBenchmark benchmark;
+  benchmark.runBenchmark(counters);
+}
+
+class SinglePointArrayGeneratorBenchmark
+    : public engine::util::NetworkedBenchmark {
+ public:
+  void setup() override {
+    auto [agentFactory0, agentFactory1] =
+        engine::util::getSocketAgentFactories();
+    agentFactory0_ = std::move(agentFactory0);
+    agentFactory1_ = std::move(agentFactory1);
+
+    size_t width = std::ceil(std::log2(length_));
+    size_t batchSize = 128;
+
+    party0Input_ =
+        std::vector<std::vector<bool>>(width, std::vector<bool>(batchSize));
+    party1Input_ =
+        std::vector<std::vector<bool>>(width, std::vector<bool>(batchSize));
+    for (size_t i = 0; i < batchSize; i++) {
+      auto [share0, share1, _] =
+          util::generateSharedRandomBoolVectorForSinglePointArrayGenerator(
+              length_);
+      for (size_t j = 0; j < width; j++) {
+        party0Input_[j][i] = share0.at(j);
+        party1Input_[j][i] = share1.at(j);
+      }
+    }
+  }
+
+ protected:
+  void initSender() override {
+    scheduler::SchedulerKeeper<0>::setScheduler(
+        scheduler::createLazySchedulerWithRealEngine(0, *agentFactory0_));
+    SinglePointArrayGeneratorFactory factory(
+        true, std::make_unique<ObliviousDeltaCalculatorFactory<0>>(true, 0, 1));
+    sender_ = factory.create();
+  }
+
+  void runSender() override {
+    sender_->generateSinglePointArrays(party0Input_, length_);
+  }
+
+  void initReceiver() override {
+    scheduler::SchedulerKeeper<1>::setScheduler(
+        scheduler::createLazySchedulerWithRealEngine(1, *agentFactory1_));
+    SinglePointArrayGeneratorFactory factory(
+        false,
+        std::make_unique<ObliviousDeltaCalculatorFactory<1>>(false, 0, 1));
+    receiver_ = factory.create();
+  }
+
+  void runReceiver() override {
+    receiver_->generateSinglePointArrays(party1Input_, length_);
+  }
+
+  std::pair<uint64_t, uint64_t> getTrafficStatistics() override {
+    return scheduler::SchedulerKeeper<0>::getTrafficStatistics();
+  }
+
+ private:
+  size_t length_ = 16384;
+
+  std::unique_ptr<engine::communication::IPartyCommunicationAgentFactory>
+      agentFactory0_;
+  std::unique_ptr<engine::communication::IPartyCommunicationAgentFactory>
+      agentFactory1_;
+
+  std::unique_ptr<ISinglePointArrayGenerator> sender_;
+  std::unique_ptr<ISinglePointArrayGenerator> receiver_;
+
+  std::vector<std::vector<bool>> party0Input_;
+  std::vector<std::vector<bool>> party1Input_;
+};
+
+BENCHMARK_COUNTERS(SinglePointArrayGenerator_Benchmark, counters) {
+  SinglePointArrayGeneratorBenchmark benchmark;
   benchmark.runBenchmark(counters);
 }
 } // namespace fbpcf::mpc_std_lib::oram
