c2a357e82a66905a3e0d2db0bd4ddb0e41299bf5
Elliott Lawrence <elliottlawrence@fb.com>
2022-03-22 14:23:26 -0700

    Benchmark for DifferenceCalculator (#124)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/124
    
    Same idea as previous diffs. Just going through the ORAM components now.
    
    Reviewed By: chualynn
    
    Differential Revision: D34915078
    
    fbshipit-source-id: c79782d7aae85c8650ac1205bf51071f8ad42fa8

--- fbpcf/mpc_std_lib/util/test/util.h
+++ fbpcf/mpc_std_lib/util/test/util.h
@@ -14,7 +14,6 @@
 #include <stdexcept>
 
 #include "fbpcf/mpc_std_lib/util/util.h"
-#include "fbpcf/test/TestHelper.h"
 
 namespace fbpcf::mpc_std_lib::util {
 // the 3 returned valeus are: true value, first share, second share
@@ -52,6 +51,63 @@ getRandomData<AggregationValue>(std::mt19937_64& e) {
   valueShare1.conversionValue =
       value.conversionValue ^ valueShare0.conversionValue;
   return {value, valueShare0, valueShare1};
+}
+
+template <typename T>
+struct InputType {
+  std::vector<uint32_t> indicatorShares;
+  std::vector<std::vector<bool>> minuendShares;
+  std::vector<T> subtrahendShares;
+};
+
+template <typename T, int indicatorWidth>
+std::tuple<InputType<T>, InputType<T>, std::vector<T>> generateRandomInputs(
+    size_t batchSize) {
+  InputType<T> rst0{
+      .indicatorShares = std::vector<uint32_t>(batchSize),
+      .minuendShares = std::vector<std::vector<bool>>(),
+      .subtrahendShares = std::vector<T>(batchSize),
+  };
+  InputType<T> rst1{
+      .indicatorShares = std::vector<uint32_t>(batchSize),
+      .minuendShares = std::vector<std::vector<bool>>(),
+      .subtrahendShares = std::vector<T>(batchSize),
+  };
+  std::vector<T> expectedValue(batchSize);
+
+  std::random_device rd;
+  std::mt19937_64 e(rd());
+  std::uniform_int_distribution<uint32_t> randomIndicator(0, 0xFFFF);
+  std::uniform_int_distribution<int32_t> randomBit(0, 1);
+
+  for (size_t i = 0; i < batchSize; i++) {
+    auto [minuend, minuendShare0, minuendShare1] = util::getRandomData<T>(e);
+    auto subtrahend = std::get<0>(util::getRandomData<T>(e));
+    // indicator is 1 or -1;
+    int32_t indicator = static_cast<int32_t>(-1) + 2 * randomBit(e);
+    expectedValue[i] =
+        indicator == 1 ? (minuend - subtrahend) : (subtrahend - minuend);
+
+    rst0.indicatorShares[i] = randomIndicator(e);
+    rst1.indicatorShares[i] =
+        (rst0.indicatorShares.at(i) - indicator) % (1 << indicatorWidth);
+
+    rst0.subtrahendShares[i] = std::get<0>(util::getRandomData<T>(e));
+    rst1.subtrahendShares[i] = rst0.subtrahendShares.at(i) - subtrahend;
+
+    auto tmp0 = util::Adapters<T>::convertToBits(minuendShare0);
+    auto tmp1 = util::Adapters<T>::convertToBits(minuendShare1);
+    for (size_t j = 0; j < tmp0.size(); j++) {
+      if (j >= rst0.minuendShares.size()) {
+        rst0.minuendShares.push_back(std::vector<bool>(batchSize));
+        rst1.minuendShares.push_back(std::vector<bool>(batchSize));
+      }
+      rst0.minuendShares[j][i] = tmp0[j];
+      rst1.minuendShares[j][i] = tmp1[j];
+    }
+  }
+
+  return {rst0, rst1, expectedValue};
 }
 
 } // namespace fbpcf::mpc_std_lib::util
