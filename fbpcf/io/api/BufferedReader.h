dc6714b140d1c3b1405a2e17eac33b14e2d48fd5
Aditya Shastri <adshastri@fb.com>
2022-03-23 11:39:34 -0700

    Implement BufferedReader that wraps a child reader (#106)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/106
    
    This diff creates a BufferedReader that has the following functionality
    - It reads the underlying file chunk by chunk
    - It allows the caller to specify a chunk size
    - It exposes a read() function
    - It exposes an eof() function that checks whether the underlying file at eof() and there is no more data in the local buffer
    - It exposes a readLine() function
    
    Differential Revision: D34901593
    
    fbshipit-source-id: 5eb22690c9d5da35adca61d2b7f35a56c8953a43

---
+++ fbpcf/io/api/BufferedReader.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <cstddef>
+#include <memory>
+#include <vector>
+
+#include "fbpcf/io/api/IReaderCloser.h"
+
+namespace fbpcf::io {
+
+/*
+This class is the API for buffered reading, which
+provides a readLine function as well as the ability
+to specify a chunk size.
+*/
+
+constexpr size_t defaultChunkSize = 4096;
+
+class BufferedReader : public IReaderCloser {
+ public:
+  explicit BufferedReader(
+      IReaderCloser& baseReader,
+      const size_t chunkSize = defaultChunkSize)
+      : buffer_{std::vector<char>(chunkSize)},
+        currentPosition_{0},
+        baseReader_{baseReader},
+        lastPosition_{0} {}
+
+  int close() override;
+  size_t read(std::vector<char>& buf) override;
+  bool eof() override;
+  ~BufferedReader() override;
+
+  std::string readLine();
+
+ private:
+  void loadNextChunk();
+
+  std::vector<char> buffer_;
+  size_t currentPosition_;
+  IReaderCloser& baseReader_;
+  size_t lastPosition_;
+};
+
+} // namespace fbpcf::io
