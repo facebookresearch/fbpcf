babd718a39e419a8354603e0af5ea9526c44943a
Elliott Lawrence <elliottlawrence@fb.com>
2022-03-24 12:42:52 -0700

    Move scheduler benchmarks to one file (#137)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/137
    
    I'm seeing some errors about "no metric data" from the scheduler benchmark: https://www.internalfb.com/intern/servicelab/trial/603915226/
    
    It turns out all the benchmark macros need to be in one file for this to work.
    
    Reviewed By: adshastri
    
    Differential Revision: D35086855
    
    fbshipit-source-id: d49abe31c681a9b6cc207d11f533bbb7e5577cb4

--- fbpcf/scheduler/test/benchmarks/SchedulerBenchmark.cpp
+++ fbpcf/scheduler/test/benchmarks/SchedulerBenchmark.cpp
@@ -12,6 +12,83 @@
 #include "fbpcf/scheduler/test/benchmarks/AllocatorBenchmark.h"
 #include "fbpcf/scheduler/test/benchmarks/WireKeeperBenchmark.h"
 
+namespace fbpcf::scheduler {
+
+// IAllocator benchmarks
+
+BENCHMARK(VectorArenaAllocator_allocate, n) {
+  benchmarkAllocate<int64_t>(
+      std::make_unique<VectorArenaAllocator<int64_t, unsafe>>(), n);
+}
+
+BENCHMARK(VectorArenaAllocator_free, n) {
+  benchmarkFree<int64_t>(
+      std::make_unique<VectorArenaAllocator<int64_t, unsafe>>(), n);
+}
+
+BENCHMARK(VectorArenaAllocator_get, n) {
+  benchmarkGet<int64_t>(
+      std::make_unique<VectorArenaAllocator<int64_t, unsafe>>(), n);
+}
+
+BENCHMARK(UnorderedMapAllocator_allocate, n) {
+  benchmarkAllocate<int64_t>(
+      std::make_unique<UnorderedMapAllocator<int64_t>>(), n);
+}
+
+BENCHMARK(UnorderedMapAllocator_free, n) {
+  benchmarkFree<int64_t>(std::make_unique<UnorderedMapAllocator<int64_t>>(), n);
+}
+
+BENCHMARK(UnorderedMapAllocator_get, n) {
+  benchmarkGet<int64_t>(std::make_unique<UnorderedMapAllocator<int64_t>>(), n);
+}
+
+// WireKeeper benchmarks
+
+BENCHMARK(WireKeeperBenchmark_allocateBooleanValue, n) {
+  BENCHMARK_WIREKEEPER {
+    wireKeeper->allocateBooleanValue();
+  }
+}
+
+BENCHMARK(WireKeeperBenchmark_getBooleanValue, n) {
+  BENCHMARK_WIREKEEPER {
+    wireKeeper->getBooleanValue(wireIds.at(n));
+  }
+}
+
+BENCHMARK(WireKeeperBenchmark_setBooleanValue, n) {
+  BENCHMARK_WIREKEEPER {
+    wireKeeper->setBooleanValue(wireIds.at(n), n & 1);
+  }
+}
+
+BENCHMARK(WireKeeperBenchmark_getFirstAvailableLevel, n) {
+  BENCHMARK_WIREKEEPER {
+    wireKeeper->getFirstAvailableLevel(wireIds.at(n));
+  }
+}
+
+BENCHMARK(WireKeeperBenchmark_setFirstAvailableLevel, n) {
+  BENCHMARK_WIREKEEPER {
+    wireKeeper->setFirstAvailableLevel(wireIds.at(n), n);
+  }
+}
+
+BENCHMARK(WireKeeperBenchmark_increaseReferenceCount, n) {
+  BENCHMARK_WIREKEEPER {
+    wireKeeper->increaseReferenceCount(wireIds.at(n));
+  }
+}
+
+BENCHMARK(WireKeeperBenchmark_decreaseReferenceCount, n) {
+  BENCHMARK_WIREKEEPER {
+    wireKeeper->decreaseReferenceCount(wireIds.at(n));
+  }
+}
+} // namespace fbpcf::scheduler
+
 int main(int argc, char* argv[]) {
   facebook::initFacebook(&argc, &argv);
   folly::runBenchmarks();
