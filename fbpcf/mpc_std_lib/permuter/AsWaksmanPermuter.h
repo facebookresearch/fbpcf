d6eb768136460cf0ce7fbb8b35ce6b5518c24de2
RuiyuZhu <14908990+RuiyuZhu@users.noreply.github.com>
2022-03-24 16:57:09 -0700

    Waksman network parameter calculator (#145)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/145
    
    This diff has intense math. See the tech detail in the appendix of https://docs.google.com/document/d/1fvXtIeJp_6M09rB9SBi8o0UALZsbSqP_B7Q49L7AQ54/edit#.
    
    Reviewed By: robotal
    
    Differential Revision: D35068379
    
    fbshipit-source-id: 02325a27fd8ddb93aa5932bf47a5293a5fc90880

---
+++ fbpcf/mpc_std_lib/permuter/AsWaksmanPermuter.h
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "fbpcf/mpc_std_lib/permuter/IPermuter.h"
+
+#include "fbpcf/mpc_std_lib/util/util.h"
+
+namespace fbpcf::mpc_std_lib::permuter {
+
+/**
+ * This object is to calculate the parameters to use AsWaksman network to
+ * permute a set of values. The construction of the network can be found in
+ * paper: Bruno Beauquier, Eric Darrot. On Arbitrary Waksman Networks and their
+ * Vulnerability. RR-3788, INRIA. 1999. inria-00072871f
+ */
+class AsWaksmanParameterCalculator {
+ public:
+  explicit AsWaksmanParameterCalculator(const std::vector<uint32_t>& order)
+      : size_(order.size()),
+        firstHalfSize_(size_ / 2),
+        secondHalfSize_(size_ - firstHalfSize_),
+        expectedOrder_(order),
+        inversedOrder_(size_),
+        firstSwapConditions_(firstHalfSize_),
+        secondSwapConditions_(secondHalfSize_ - 1),
+        firstSubPermuteOrder_(firstHalfSize_, static_cast<uint32_t>(-1)),
+        secondSubPermuteOrder_(secondHalfSize_, static_cast<uint32_t>(-1)) {
+    for (size_t i = 0; i < size_; i++) {
+      inversedOrder_[expectedOrder_.at(i)] = i;
+    }
+    compute();
+  }
+
+  std::vector<bool> getFirstSwapConditions() {
+    return std::move(firstSwapConditions_);
+  }
+
+  std::vector<bool> getSecondSwapConditions() {
+    return std::move(secondSwapConditions_);
+  }
+
+  std::vector<uint32_t> getFirstSubPermuteOrder() {
+    return std::move(firstSubPermuteOrder_);
+  }
+
+  std::vector<uint32_t> getSecondSubPermuteOrder() {
+    return std::move(secondSubPermuteOrder_);
+  }
+
+ private:
+  void compute();
+  uint32_t fillIntoSecondHalf(uint32_t indexAfterPermute);
+
+  // find the index of the dual element
+  inline uint32_t findDualIndex(uint32_t index) const {
+    // the last element has no dual element when size is odd
+    if ((index == size_ - 1) && ((size_ % 2) == 1)) {
+      return static_cast<uint32_t>(-1);
+    }
+    if (index >= firstHalfSize_) {
+      return index - firstHalfSize_;
+    } else {
+      return index + firstHalfSize_;
+    }
+  }
+
+  // find the corresponding index in the sub permute
+  inline uint32_t findIndexInSubPermute(uint32_t index) const {
+    if (index >= firstHalfSize_) {
+      return index - firstHalfSize_;
+    } else {
+      return index;
+    }
+  }
+
+  inline uint32_t findAFreeIndex() const {
+    size_t index = secondHalfSize_ - 1;
+    while ((index < size_) && (secondSubPermuteOrder_.at(index) < size_)) {
+      index--;
+    }
+    if (index < size_) {
+      return index + firstHalfSize_;
+    } else {
+      return index;
+    }
+  }
+
+  const size_t size_;
+  const size_t firstHalfSize_;
+  const size_t secondHalfSize_;
+  const std::vector<uint32_t>& expectedOrder_;
+
+  std::vector<uint32_t> inversedOrder_;
+  std::vector<bool> firstSwapConditions_;
+  std::vector<bool> secondSwapConditions_;
+
+  std::vector<uint32_t> firstSubPermuteOrder_;
+  std::vector<uint32_t> secondSubPermuteOrder_;
+};
+
+} // namespace fbpcf::mpc_std_lib::permuter
