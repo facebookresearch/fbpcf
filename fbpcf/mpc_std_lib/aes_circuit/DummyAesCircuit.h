50945eae0bca7b5b3ae27169ae9d215dbd163ada
Ruiyu Zhu <ruiyuzhu@fb.com>
2022-04-14 17:24:53 -0700

    NonAesCircuit as a placeholder. (#189)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/189
    
    As title. This implementation do nothing but merely output the inputs. They are only meant to be used as placeholder for tests.
    
    Reviewed By: chualynn
    
    Differential Revision: D34507629
    
    fbshipit-source-id: 20764e703098b294db001a361c93b022c891b0ef

---
+++ fbpcf/mpc_std_lib/aes_circuit/DummyAesCircuit.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "fbpcf/mpc_std_lib/aes_circuit/IAesCircuit.h"
+
+namespace fbpcf::mpc_std_lib::aes_circuit::insecure {
+
+/*
+ * This is merely a placeholder that basically does nothing other than input
+ * validation. This object is only meant to be used for test purpose. When
+ * testing, the corresponding encryption/decryption process should also do
+ * nothing to make the final result correct.
+ */
+template <typename BitType>
+class DummyAesCircuit final : public IAesCircuit<BitType> {
+ private:
+  std::vector<BitType> encrypt_impl(
+      const std::vector<BitType>& plaintext,
+      const std::vector<BitType>& /* expandedEncKey */) const {
+    return plaintext;
+  }
+
+  /**
+   * @inherit doc
+   */
+  std::vector<BitType> decrypt_impl(
+      const std::vector<BitType>& ciphertext,
+      const std::vector<BitType>& /* expandedDecKey */) const {
+    return ciphertext;
+  }
+};
+
+} // namespace fbpcf::mpc_std_lib::aes_circuit::insecure
---
+++ fbpcf/mpc_std_lib/aes_circuit/IAesCircuitFactory.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "fbpcf/mpc_std_lib/aes_circuit/IAesCircuit.h"
+
+namespace fbpcf::mpc_std_lib::aes_circuit {
+
+template <typename BitType>
+class IAesCircuitFactory {
+ public:
+  virtual ~IAesCircuitFactory() = default;
+  virtual std::unique_ptr<IAesCircuit<BitType>> create() = 0;
+
+  enum CircuitType {
+    Dummy,
+    Secure,
+  };
+
+  virtual CircuitType getCircuitType() const = 0;
+};
+
+} // namespace fbpcf::mpc_std_lib::aes_circuit
