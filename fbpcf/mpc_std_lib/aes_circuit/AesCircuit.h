00b33f699c92ec4d4f6804b1b83a21d95d986a23
Ruiyu Zhu <ruiyuzhu@fb.com>
2022-04-14 17:24:53 -0700

    AES circuit APIs (#190)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/190
    
    This diff creates the APIs for AES components
    
    Reviewed By: chualynn
    
    Differential Revision: D35151922
    
    fbshipit-source-id: c052803f528375b87a80b7141adcf5f6d62c36d1

---
+++ fbpcf/mpc_std_lib/aes_circuit/AesCircuit.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "fbpcf/mpc_std_lib/aes_circuit/IAesCircuit.h"
+
+namespace fbpcf::mpc_std_lib::aes_circuit {
+
+/*
+ * This is the implementation of AES circuit
+ */
+template <typename BitType>
+class AesCircuit : public IAesCircuit<BitType> {
+ private:
+  std::vector<BitType> encrypt_impl(
+      const std::vector<BitType>& plaintext,
+      const std::vector<BitType>& expandedEncKey) const;
+
+  using ByteType = std::array<BitType, 8>;
+  using WordType = std::array<ByteType, 4>;
+  /**
+   * @inherit doc
+   */
+  std::vector<BitType> decrypt_impl(
+      const std::vector<BitType>& ciphertext,
+      const std::vector<BitType>& expandedDecKey) const;
+
+ protected:
+  std::vector<std::array<WordType, 4>> convertToWords(
+      const std::vector<BitType>& src) const;
+
+  std::vector<BitType> convertFromWords(
+      std::vector<std::array<WordType, 4>>& src) const;
+
+  void sBoxInPlace(ByteType& src) const;
+  void inverseSBoxInPlace(ByteType& src) const;
+
+  void mixColumnsInPlace(WordType& src) const;
+  void inverseMixColumnsInPlace(WordType& src) const;
+
+  void shiftRowInPlace(WordType& src, int8_t offset);
+};
+
+} // namespace fbpcf::mpc_std_lib::aes_circuit
