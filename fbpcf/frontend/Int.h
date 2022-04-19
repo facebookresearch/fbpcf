879f86f1df74528bd16061c912f04cc285bdf75e
Tarek Elgamal <telgamal@fb.com>
2022-04-19 10:22:11 -0700

    Implement the min function for integer type (#191)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/191
    
    Implement min function for Int type. Added unittest (IntTest.testMin)
    
    Reviewed By: adshastri
    
    Differential Revision: D35637118
    
    fbshipit-source-id: 22c343edf7c67e4869c589b6935f13883f9949c3

--- fbpcf/frontend/Int.h
+++ fbpcf/frontend/Int.h
@@ -290,6 +290,17 @@ Int<isSigned, width, isSecret1 || isSecret2, schedulerId, usingBatch> max(
     const Int<isSigned, width, isSecret1, schedulerId, usingBatch>& left,
     const Int<isSigned, width, isSecret2, schedulerId, usingBatch>& right);
 
+template <
+    bool isSigned,
+    int8_t width,
+    bool isSecret1,
+    bool isSecret2,
+    int schedulerId,
+    bool usingBatch>
+Int<isSigned, width, isSecret1 || isSecret2, schedulerId, usingBatch> min(
+    const Int<isSigned, width, isSecret1, schedulerId, usingBatch>& left,
+    const Int<isSigned, width, isSecret2, schedulerId, usingBatch>& right);
+
 } // namespace fbpcf::frontend
 
 #include "fbpcf/frontend/Int_impl.h"
