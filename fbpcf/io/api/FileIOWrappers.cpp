7e4e665d629d68d342519dd348da3d59a8749af9
Bill Kao <billkao@fb.com>
2022-06-21 14:34:08 -0700

    Create A FileIOWrappers file (#229)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/229
    
    Create a FileIO Wrappers class with `readFile`, `writeFile` and `transferFileInParts`. All the three functions use the `BufferedWriter` and `BufferedReader` underneath.
    
    The BufferedWriter and BufferedReader will soon support GCS file path. This Wrapper file will be use as utility class to replace the the current `fbpcf::io::read` and `fbpcf::io::write` to enable the GCS functionality.
    
    Differential Revision: D37153814
    
    fbshipit-source-id: 90b6361a5c49a97c8452a17bec4bde4f3296220f

---
+++ fbpcf/io/api/FileIOWrappers.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <cstddef>
+#include <string>
+
+#include "fbpcf/io/api/BufferedReader.h"
+#include "fbpcf/io/api/BufferedWriter.h"
+#include "fbpcf/io/api/FileIOWrappers.h"
+#include "fbpcf/io/api/FileReader.h"
+#include "fbpcf/io/api/FileWriter.h"
+
+namespace fbpcf::io {
+
+std::string FileIOWrappers::readFile(const std::string& srcPath) {
+  auto reader = std::make_unique<fbpcf::io::FileReader>(srcPath);
+  auto bufferedReader = std::make_unique<fbpcf::io::BufferedReader>(
+      std::move(reader), kBufferedReaderChunkSize);
+  std::string output = "";
+  std::string newLine = "\n";
+  while (!bufferedReader->eof()) {
+    auto line = bufferedReader->readLine();
+    output += line + newLine;
+  }
+  bufferedReader->close();
+  return output;
+}
+
+void FileIOWrappers::writeFile(
+    const std::string& destPath,
+    std::string& content) {
+  auto fileWriter = std::make_unique<fbpcf::io::FileWriter>(destPath);
+  auto bufferedWriter = std::make_unique<fbpcf::io::BufferedWriter>(
+      std::move(fileWriter), kBufferedWriterChunkSize);
+  bufferedWriter->writeString(content);
+  bufferedWriter->close();
+}
+
+void FileIOWrappers::transferFileInParts(
+    const std::string& srcPath,
+    const std::string& destPath) {
+  auto fileWriter = std::make_unique<fbpcf::io::FileWriter>(destPath);
+  auto bufferedWriter = std::make_unique<fbpcf::io::BufferedWriter>(
+      std::move(fileWriter), kBufferedWriterChunkSize);
+
+  auto reader = std::make_unique<fbpcf::io::FileReader>(srcPath);
+  auto bufferedReader = std::make_unique<fbpcf::io::BufferedReader>(
+      std::move(reader), kBufferedReaderChunkSize);
+
+  std::string newLine = "\n";
+  while (!bufferedReader->eof()) {
+    auto line = bufferedReader->readLine();
+    bufferedWriter->writeString(line);
+    bufferedWriter->writeString(newLine);
+  }
+
+  bufferedReader->close();
+  bufferedWriter->close();
+}
+
+} // namespace fbpcf::io
---
+++ fbpcf/io/api/test/FileIOWrappersTest.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "fbpcf/io/api/FileIOWrappers.h"
+#include <folly/Format.h>
+#include <gtest/gtest.h>
+#include <stdio.h>
+#include <filesystem>
+#include <random>
+#include <string>
+#include "fbpcf/io/api/test/utils/IOTestHelper.h"
+#include "folly/Random.h"
+
+namespace fbpcf::io {
+
+TEST(FileIOWrappersTest, testTransferFileInParts) {
+  std::string baseDir = IOTestHelper::getBaseDirFromPath(__FILE__);
+  std::string fileToWriteTo = folly::sformat(
+      "{}data/file_io_wrapper_transfer_in_parts_test_file_{}.txt",
+      baseDir,
+      folly::Random::secureRand64());
+  std::string expectedFile =
+      baseDir + "data/expected_file_io_wrapper_transfer_in_parts_test_file.txt";
+
+  fbpcf::io::FileIOWrappers::transferFileInParts(expectedFile, fileToWriteTo);
+
+  /*
+    Verify that file contents match the expected
+  */
+  IOTestHelper::expectFileContentsMatch(fileToWriteTo, expectedFile);
+  IOTestHelper::cleanup(fileToWriteTo);
+}
+
+TEST(FileIOWrappersTest, testWriteFile) {
+  std::string baseDir = IOTestHelper::getBaseDirFromPath(__FILE__);
+  std::string fileToWriteTo = folly::sformat(
+      "{}data/file_io_wrapper_transfer_in_parts_test_file_{}.txt",
+      baseDir,
+      folly::Random::secureRand64());
+  std::string fileToRead = baseDir + "data/file_io_wrappers_read_file_test.txt";
+
+  std::string content =
+      "this is a test file\n"
+      "here's the second line, please read me\n"
+      "\n"
+      "last line here, don't miss me ~~\n";
+  fbpcf::io::FileIOWrappers::writeFile(fileToWriteTo, content);
+
+  IOTestHelper::expectFileContentsMatch(fileToWriteTo, fileToRead);
+  IOTestHelper::cleanup(fileToWriteTo);
+}
+
+TEST(FileIOWrappersTest, testReadFile) {
+  std::string baseDir = IOTestHelper::getBaseDirFromPath(__FILE__);
+  std::string fileToRead = baseDir + "data/file_io_wrappers_read_file_test.txt";
+
+  std::string content = fbpcf::io::FileIOWrappers::readFile(fileToRead);
+
+  std::string expectedContent =
+      "this is a test file\n"
+      "here's the second line, please read me\n"
+      "\n"
+      "last line here, don't miss me ~~\n";
+  EXPECT_EQ(content, expectedContent);
+}
+
+} // namespace fbpcf::io
