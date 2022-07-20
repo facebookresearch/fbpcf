b5b5b1b51983e3b965f80d744472f2288183467e
Tal Davidi <tdavidi@fb.com>
2022-07-20 11:06:47 -0700

    Implement edit distance business Logic (#286)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/286
    
    Implements the core logic to calculate the edit distance. The algorithm is slightly complicated by the fact that the size of the strings is unknown, so the solution matrix must account for that.
    
    {F753583602}
    
    I drew the following diagram as an example. The rows direction has the letters of the word, and the columns are the letters of the guess that we are trying to change into the word.
    
    Moving up means we are inserting the letter into the guess at that position of the word. Moving left means we are deleting the letter of the guess at the current position. Moving diagonally means replacement of the letter.
    
    Once we are out of bounds, the logic depends on which quadrant we are in. Assuming the quadrants are numbered
    1 2
    3 4
    quadrant 2 indicates we should copy the value to the left, quadrant 3 is copying the value from above, and quadrant 4 is copying the value diagonally up and left. This guarantees that the correct solution in the position of the last letter of guess and last letter of word will surround quadrant 4, and then be copied into the last position.
    
    Reviewed By: RuiyuZhu
    
    Differential Revision: D37792933
    
    fbshipit-source-id: 0291240247b813147bf2c496e0b2c1db1cd3f949

---
+++ example/edit_distance/EditDistanceCalculator_impl.h
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "./EditDistanceCalculator.h" // @manual
+
+namespace fbpcf::edit_distance {
+
+template <int schedulerId>
+void EditDistanceCalculator<schedulerId>::calculateEditDistances() {
+  const SecString<schedulerId>& words = inputProcessor_.getWords();
+  const SecString<schedulerId>& guesses = inputProcessor_.getGuesses();
+
+  SecUChar<schedulerId> wordsSize = words.template privateSize<charLength>();
+  SecUChar<schedulerId> guessSize = guesses.template privateSize<charLength>();
+
+  size_t batchSize = inputProcessor_.getInputData().getNumRows();
+  Pub32Int<schedulerId> zero =
+      createPublicBatchConstant<Pub32Int<schedulerId>, int64_t>(0, batchSize);
+  const Pub32Int<schedulerId>& insertCost = inputProcessor_.getInsertCost();
+  const Pub32Int<schedulerId>& deleteCost = inputProcessor_.getDeleteCost();
+
+  std::vector<SecBool<schedulerId>> in_bounds_word(maxStringLength);
+  std::vector<SecBool<schedulerId>> in_bounds_guess(maxStringLength);
+  for (size_t i = 0; i < maxStringLength; i++) {
+    PubUChar<schedulerId> index =
+        createPublicBatchConstant<PubUChar<schedulerId>, size_t>(i, batchSize);
+    in_bounds_word[i] = index < wordsSize;
+    in_bounds_guess[i] = index < guessSize;
+  }
+
+  std::vector<std::vector<Sec32Int<schedulerId>>> distances(
+      maxStringLength + 1);
+
+  distances[0] = std::vector<Sec32Int<schedulerId>>(maxStringLength + 1);
+
+  // Not technically a secret value but makes the types work more easily
+  distances[0][0] = createSecretBatchConstant<Sec32Int<schedulerId>, int64_t>(
+      0, batchSize, PLAYER0);
+
+  for (size_t i = 1; i <= maxStringLength; i++) {
+    distances[i] = std::vector<Sec32Int<schedulerId>>(maxStringLength + 1);
+    distances[0][i] =
+        distances[0][i - 1] + zero.mux(in_bounds_guess[i - 1], deleteCost);
+    distances[i][0] =
+        distances[i - 1][0] + zero.mux(in_bounds_word[i - 1], insertCost);
+  }
+
+  for (size_t i = 1; i <= maxStringLength; i++) {
+    for (size_t j = 1; j <= maxStringLength; j++) {
+      // cast signed char to signed 32int
+      SecChar<schedulerId> wordLetter = words[i - 1];
+      SecChar<schedulerId> guessLetter = guesses[j - 1];
+      Sec32Int<schedulerId> asciiDist =
+          (wordLetter - guessLetter)
+              .mux(wordLetter < guessLetter, guessLetter - wordLetter)
+              .template cast<int32Length>();
+      auto replaceSol = distances[i - 1][j - 1] + asciiDist;
+      auto insertSol = distances[i - 1][j] + insertCost;
+      auto deleteSol = distances[i][j - 1] + deleteCost;
+      auto minSol = fbpcf::frontend::min(
+          fbpcf::frontend::min(replaceSol, insertSol), deleteSol);
+      /*
+      Case 1: In Bounds Both Words
+          - Return minSolution
+      Case 2: In Bounds of Guess / Not in bounds of word
+          - Copy solution of wordIndex - 1
+      Case 3: In bounds of Word / Not in bounds of guess
+          - Copy solution of guessIndex - 1
+      Case 4: Not in bounds of either
+          - Copy solution of wordIndex - 1, guessIndex - 1
+      */
+      distances[i][j] =
+          distances[i - 1][j - 1]
+              .mux(in_bounds_word[i - 1], distances[i][j - 1])
+              .mux(
+                  in_bounds_guess[j - 1],
+                  distances[i - 1][j].mux(
+                      in_bounds_word[i - 1] & in_bounds_guess[j - 1], minSol));
+    }
+  }
+
+  editDistances_ = distances[maxStringLength][maxStringLength];
+}
+
+template <int schedulerId>
+void EditDistanceCalculator<schedulerId>::calculateMessages() {
+  const Pub32Int<schedulerId>& threshold = inputProcessor_.getThreshold();
+  SecUChar<schedulerId> messageLength =
+      inputProcessor_.getSenderMessages().template privateSize<charLength>();
+
+  size_t batchSize = inputProcessor_.getInputData().getNumRows();
+  PubString<schedulerId> emptyMessage =
+      createPublicBatchConstant<PubString<schedulerId>, std::string>(
+          "", batchSize);
+  PubChar<schedulerId> nullChar =
+      createPublicBatchConstant<PubChar<schedulerId>, int64_t>(0, batchSize);
+  PubUChar<schedulerId> one =
+      createPublicBatchConstant<PubUChar<schedulerId>, uint64_t>(1, batchSize);
+  const SecString<schedulerId>& senderMessage =
+      inputProcessor_.getSenderMessages();
+  SecString<schedulerId> halfMessage;
+  for (size_t i = 0; i < maxStringLength; i++) {
+    PubUChar<schedulerId> index =
+        createPublicBatchConstant<PubUChar<schedulerId>, uint64_t>(
+            i, batchSize);
+    halfMessage[i] =
+        nullChar.mux((index + index + one) < messageLength, senderMessage[i]);
+  }
+
+  receiverMessages_ =
+      emptyMessage.mux(editDistances_ <= threshold + threshold, halfMessage)
+          .mux(editDistances_ < threshold, senderMessage);
+}
+} // namespace fbpcf::edit_distance
