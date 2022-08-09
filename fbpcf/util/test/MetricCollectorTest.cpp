89bb39769cda4709d17198e63b2083610c877bfd
Yan Dong <dongyan@fb.com>
2022-08-09 13:18:22 -0700

    Add an API to generate aggregated metrics (#328)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/328
    
    See task: https://www.internalfb.com/tasks/?t=124977653
    
    Reviewed By: RuiyuZhu
    
    Differential Revision: D38546755
    
    fbshipit-source-id: 806d8d93e18524111309ffe193e457bcb1b46ba9

--- fbpcf/util/test/MetricCollectorTest.cpp
+++ fbpcf/util/test/MetricCollectorTest.cpp
@@ -46,4 +46,36 @@ TEST(MetricCollectorTest, testMetricCollector) {
   EXPECT_EQ(expectedOutcome, outcome);
 }
 
+TEST(MetricCollectorTest, testMetricAggregate) {
+  MetricCollector collector("test");
+
+  folly::dynamic metric1 =
+      folly::dynamic::object("time", 3)("speed", 2)("price", 100.1);
+  folly::dynamic metric2 =
+      folly::dynamic::object("volume", "high")("price", 99.3)("speed", 5);
+  folly::dynamic expectedOutcome = folly::dynamic::object("test.time", 3)(
+      "test.speed", 7)("test.price", 199.4);
+  {
+    std::shared_ptr<IMetricRecorder> r1 =
+        std::make_shared<TestMetricRecorder>(metric1);
+    std::shared_ptr<IMetricRecorder> r2 =
+        std::make_shared<TestMetricRecorder>(metric2);
+
+    collector.addNewRecorder("traffic", r1);
+    collector.addNewRecorder("liquid", r2);
+    // the recorders are going out of scope.
+  }
+
+  auto outcome = collector.aggregateMetrics();
+  EXPECT_EQ(expectedOutcome.size(), outcome.size());
+
+  for (auto& [k, v] : outcome.items()) {
+    if (v.isDouble()) {
+      EXPECT_DOUBLE_EQ(expectedOutcome[k].asDouble(), v.asDouble());
+    } else {
+      EXPECT_EQ(expectedOutcome[k], v);
+    }
+  }
+}
+
 } // namespace fbpcf::util
