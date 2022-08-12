ad3e1950a9bc3da06075ca5204617144aa1e863f
Hao Chen (Signals) <haoche@fb.com>
2022-08-12 15:58:22 -0700

    Add Plaintext Scheduler Factory (#330)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/330
    
    ^ see title
    
    Differential Revision: D38628988
    
    fbshipit-source-id: 64017f436b84d381ecbf998c47443661d2f02d69

---
+++ fbpcf/scheduler/PlaintextSchedulerFactory.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "fbpcf/scheduler/ISchedulerFactory.h"
+#include "fbpcf/scheduler/PlaintextScheduler.h"
+#include "fbpcf/scheduler/WireKeeper.h"
+
+namespace fbpcf::scheduler {
+
+template <bool unsafe>
+class PlaintextSchedulerFactory final : public ISchedulerFactory<unsafe> {
+ public:
+  std::unique_ptr<IScheduler> create() override {
+    return std::make_unique<PlaintextScheduler>(
+        WireKeeper::createWithVectorArena<unsafe>());
+  }
+};
+
+} // namespace fbpcf::scheduler
