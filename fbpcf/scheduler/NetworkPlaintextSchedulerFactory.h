809d34ec8762e013137e2fbb0ea0a67a7608898e
Hao Chen (Signals) <haoche@fb.com>
2022-08-15 09:39:32 -0700

    Add Network plaintext scheduler factory (#331)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/331
    
    ^
    
    Reviewed By: adshastri
    
    Differential Revision: D38628986
    
    fbshipit-source-id: 9d19fe7358910bf1d18f6247aefc441e4f6fddb9

---
+++ fbpcf/scheduler/NetworkPlaintextSchedulerFactory.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "fbpcf/engine/communication/AgentMapHelper.h"
+#include "fbpcf/engine/communication/IPartyCommunicationAgentFactory.h"
+#include "fbpcf/scheduler/ISchedulerFactory.h"
+#include "fbpcf/scheduler/NetworkPlaintextScheduler.h"
+#include "fbpcf/scheduler/WireKeeper.h"
+
+namespace fbpcf::scheduler {
+
+template <bool unsafe>
+class NetworkPlaintextSchedulerFactory final
+    : public ISchedulerFactory<unsafe> {
+ public:
+  NetworkPlaintextSchedulerFactory(
+      int myId,
+      engine::communication::IPartyCommunicationAgentFactory&
+          communicationAgentFactory)
+      : myId_(myId), communicationAgentFactory_(communicationAgentFactory) {}
+
+  std::unique_ptr<IScheduler> create() override {
+    auto numberOfParties = 2;
+    auto agentMap = engine::communication::getAgentMap(
+        numberOfParties, myId_, communicationAgentFactory_);
+
+    return std::make_unique<NetworkPlaintextScheduler>(
+        myId_,
+        std::move(agentMap),
+        WireKeeper::createWithVectorArena<unsafe>());
+  }
+
+ private:
+  int myId_;
+  engine::communication::IPartyCommunicationAgentFactory&
+      communicationAgentFactory_;
+};
+
+} // namespace fbpcf::scheduler
