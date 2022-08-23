6364c1280e2d115c8bace07741bcd047721c259c
Hao Chen (Signals) <haoche@fb.com>
2022-08-23 16:24:53 -0700

    Add helper functions to create scheduler factories (#348)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/348
    
    We add helper functions to create scheduler factories, given a scheduler type and a newly added "engine type" enum.
    
    In next diffs, will use these helper functions to gradually replace calls to create.*Scheduler.* methods in the codebase.
    
    Reviewed By: RuiyuZhu
    
    Differential Revision: D38799211
    
    fbshipit-source-id: 180fe56307e6696cbc34a3978e5f5cb4c2351d8a

--- fbpcf/scheduler/LazySchedulerFactory.h
+++ fbpcf/scheduler/LazySchedulerFactory.h
@@ -8,6 +8,8 @@
 #pragma once
 
 #include "fbpcf/engine/ISecretShareEngineFactory.h"
+#include "fbpcf/engine/SecretShareEngineFactory.h"
+#include "fbpcf/engine/communication/IPartyCommunicationAgentFactory.h"
 #include "fbpcf/scheduler/ISchedulerFactory.h"
 #include "fbpcf/scheduler/LazyScheduler.h"
 #include "fbpcf/scheduler/WireKeeper.h"
@@ -18,21 +20,62 @@ namespace fbpcf::scheduler {
 template <bool unsafe>
 class LazySchedulerFactory final : public ISchedulerFactory<unsafe> {
  public:
-  LazySchedulerFactory(engine::ISecretShareEngineFactory& engineFactory)
-      : engineFactory_(engineFactory) {}
+  LazySchedulerFactory(
+      std::unique_ptr<fbpcf::engine::ISecretShareEngineFactory> engineFactory)
+      : engineFactory_(std::move(engineFactory)) {}
 
   std::unique_ptr<IScheduler> create() override {
     std::shared_ptr<IWireKeeper> wireKeeper =
         WireKeeper::createWithVectorArena<unsafe>();
 
     return std::make_unique<LazyScheduler>(
-        engineFactory_.create(),
+        engineFactory_->create(),
         wireKeeper,
         std::make_unique<GateKeeper>(wireKeeper));
   }
 
  private:
-  engine::ISecretShareEngineFactory& engineFactory_;
+  std::unique_ptr<fbpcf::engine::ISecretShareEngineFactory> engineFactory_;
 };
+
+template <bool unsafe>
+inline std::unique_ptr<LazySchedulerFactory<unsafe>>
+getLazySchedulerFactoryWithInsecureEngine(
+    int myId,
+    engine::communication::IPartyCommunicationAgentFactory&
+        communicationAgentFactory) {
+  std::unique_ptr<engine::ISecretShareEngineFactory> engineFactory =
+      engine::getInsecureEngineFactoryWithDummyTupleGenerator(
+          myId, 2, communicationAgentFactory);
+
+  return std::make_unique<LazySchedulerFactory<unsafe>>(
+      std::move(engineFactory));
+}
+
+inline std::unique_ptr<LazySchedulerFactory</* unsafe */ true>>
+getLazySchedulerFactoryWithClassicOT(
+    int myId,
+    engine::communication::IPartyCommunicationAgentFactory&
+        communicationAgentFactory) {
+  std::unique_ptr<engine::ISecretShareEngineFactory> engineFactory =
+      engine::getSecureEngineFactoryWithClassicOt<bool>(
+          myId, 2, communicationAgentFactory);
+
+  return std::make_unique<LazySchedulerFactory</* unsafe */ true>>(
+      std::move(engineFactory));
+}
+
+inline std::unique_ptr<LazySchedulerFactory</* unsafe */ true>>
+getLazySchedulerFactoryWithRealEngine(
+    int myId,
+    engine::communication::IPartyCommunicationAgentFactory&
+        communicationAgentFactory) {
+  std::unique_ptr<engine::ISecretShareEngineFactory> engineFactory =
+      engine::getSecureEngineFactoryWithFERRET<bool>(
+          myId, 2, communicationAgentFactory);
+
+  return std::make_unique<LazySchedulerFactory</* unsafe */ true>>(
+      std::move(engineFactory));
+}
 
 } // namespace fbpcf::scheduler
