c338e8604fa2631aacff506e27575f22a43f2a93
Aditya Shastri <adshastri@fb.com>
2022-08-25 12:03:47 -0700

    Delete all instances of computing chunk size per-callsite (#1506)
    Summary:
    X-link: https://github.com/facebookresearch/fbpcs/pull/1506
    
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/369
    
    # Context
    We want to have more intelligent defaults for the IO APIs. Cloud related upload/downlaod should have a larger buffer size to minimize IO operations, whereas local io should be smaller.
    
    # This Diff
    We get rid of all instances where a custom chunk size is specified. This is only necessary if there is something special about the callsite.
    
    # This Stack
    1. Create separate constructor without chunkSize
    2. Add a filepath member variable to IWriter and IReader
    3. Create util functions to determine buffer size
    4. Use the new util functions
    5. **Delete all other references to determining chunk sizes**
    
    Reviewed By: nguytc
    
    Differential Revision: D38954973
    
    fbshipit-source-id: e433164d2e24a01b46fc550d7f0dd679df807796

--- fbpcf/io/api/FileIOWrappers.h
+++ fbpcf/io/api/FileIOWrappers.h
@@ -13,20 +13,6 @@
 
 namespace fbpcf::io {
 
-// This chunk size has to be large enough that we don't make
-// unnecessary trips to cloud storage but small enough that
-// we don't cause OOM issues. This chunk size was chosen based
-// on the size of our containers as well as the expected size
-// of our files to fit the aforementioned constraints.
-constexpr size_t kBufferedReaderChunkSize = 1'073'741'824; // 2^30
-
-// The chunk size for writing to cloud storage (S3 and GCS) must be greater than
-// 5 MB per the AWS and GCS documentation. Otherwise multipart upload will fail.
-// AWS Doc: https://docs.aws.amazon.com/AmazonS3/latest/userguide/qfacts.html
-// GCS Doc: https://cloud.google.com/storage/quotas#requests
-// The number below is 5 MB in bytes.
-constexpr size_t kBufferedWriterChunkSize = 5'242'880;
-
 // This class provides wrappers to the raw APIs to
 // do common operations like upload an entire file or
 // retrieve an entire file.
