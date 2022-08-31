a4532175da8aaad6c8f868f57b5d83b6654b3de9
Aining Liu <ainingliu@fb.com>
2022-08-30 21:31:16 -0700

    Hook up arithmetic tuple generator to secret share engine (#373)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/373
    
    Replace the stub implementation from milestone 1 in the secret share engine to utilize actual arithmetic tuples.
    
    Create nullTupleGenerator so we could pass it in without initializing FERRET whenever AND operation is unused in the circuit. Use the nullTupleGenerator when testing non-free MULT.
    
    getSecureEngineFactoryWithFERRET():configure the secret share engine with only the boolean tuple generator.
    
    getSecureEngineFactoryWithIntegerTupleGenerator(): configure the secret share engine with only the integer tuple generator.
    
    getSecureEngineFactoryWithBooleanAndIntegerTupleGenerator(): configure the secret share engine with both a boolean tuple generator and an arithmetic tuple generator.
    
    Reviewed By: robotal
    
    Differential Revision: D38999537
    
    fbshipit-source-id: 899f2569f086b6f81727e70c24d4d2008379b037

---
+++ fbpcf/engine/tuple_generator/NullTupleGeneratorFactory.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+#include "fbpcf/engine/tuple_generator/ITupleGeneratorFactory.h"
+#include "fbpcf/engine/tuple_generator/NullTupleGenerator.h"
+
+namespace fbpcf::engine::tuple_generator {
+
+/**
+ * This factory creates null tuple generators
+ */
+
+class NullTupleGeneratorFactory final : public ITupleGeneratorFactory {
+ public:
+  /**
+   * Create a null tuple generator;
+   */
+  std::unique_ptr<ITupleGenerator> create() override {
+    return std::make_unique<NullTupleGenerator>();
+  }
+};
+
+} // namespace fbpcf::engine::tuple_generator
