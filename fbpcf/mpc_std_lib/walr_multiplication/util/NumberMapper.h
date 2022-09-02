106dde1cb348229fc48a9919f4495d5b65a1d4f7
Xiangyu Guo <xyguo@fb.com>
2022-09-01 17:48:17 -0700

    Implementing the NumberMapper that maps a given number to an integer group and back.
    Differential Revision: https://internalfb.com/D39062241
    
    fbshipit-source-id: b0e7433c351036f83972206aa33223d43daecde1

--- fbpcf/mpc_std_lib/walr_multiplication/util/NumberMapper.h
+++ fbpcf/mpc_std_lib/walr_multiplication/util/NumberMapper.h
@@ -7,10 +7,11 @@
 
 #pragma once
 
-#include <limits.h>
-#include <sys/types.h>
 #include <cstdint>
+#include <limits>
 #include <stdexcept>
+#include <vector>
+#include "folly/logging/xlog.h"
 
 namespace fbpcf::mpc_std_lib::walr::util {
 
@@ -28,23 +29,61 @@ class NumberMapper {
           "The divisor's value should not exceed the max value a FixedPointType can represent.");
     }
   }
+  static constexpr double groupSize =
+      double(std::numeric_limits<FixedPointType>::max()) + 1;
 
-  // Methods supporting converting from/to FixedPointType
-  inline FixedPointType mapToFixedPointType(double input) const;
+  /*
+   * Methods supporting converting from/to FixedPointType.
+   * Given an nonnegative double input, mapToDouble(mapToFixedPointType(input))
+   * preserves the input (up to precision loss).
+   * (1) When abs(input) >= 1.0, the RELATIVE precision loss will be
+   * less than abs(input) / divisor.
+   * (2) When abs(input) < 1.0, the ABSOLUTE precision loss will be
+   * less than 1 / divisor.
+   */
+  inline FixedPointType mapToFixedPointType(double input) const {
+    double product = input * divisor_;
+    if (std::abs(product) > groupSize) {
+      XLOG_EVERY_MS(WARN, 500) << "Magnitude of input number " << input
+                               << " too large. Conversion exceeds group size."
+                               << " May incur unwanted precision loss.";
+    }
+    uintmax_t rst = static_cast<uintmax_t>(static_cast<intmax_t>(product));
+    return static_cast<FixedPointType>(rst);
+  }
 
   std::vector<FixedPointType> mapToFixedPointType(
-      const std::vector<double>& input) const;
+      const std::vector<double>& input) const {
+    std::vector<FixedPointType> rst(input.size());
+    std::transform(input.cbegin(), input.cend(), rst.begin(), [this](double a) {
+      return mapToFixedPointType(a);
+    });
+    return rst;
+  }
 
-  inline double mapToDouble(FixedPointType input) const;
+  inline double mapToDouble(FixedPointType input) const {
+    return input / static_cast<double>(divisor_);
+  }
 
   std::vector<double> mapToDouble(
-      const std::vector<FixedPointType>& input) const;
+      const std::vector<FixedPointType>& input) const {
+    std::vector<double> rst(input.size());
+    std::transform(
+        input.cbegin(), input.cend(), rst.begin(), [this](FixedPointType a) {
+          return mapToDouble(a);
+        });
+    return rst;
+  }
 
   uint64_t getDivisor() const {
     return divisor_;
   }
 
   void setDivisor(uint64_t divisor) {
+    if (divisor > std::numeric_limits<FixedPointType>::max()) {
+      throw std::invalid_argument(
+          "The divisor's value should not exceed the max value a FixedPointType can represent.");
+    }
     divisor_ = divisor;
   }
 
