eb4f4ee0bf0eafdcf3cebdb8a40dbef59943ce12
Xiangyu Guo <xyguo@fb.com>
2022-09-01 17:40:29 -0700

    Implementing the NumberMapper that maps a given number to an integer group and back. (#381)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/381
    
    This diff implements the `NumberMapper` class that maps a given `double` number to an integer group.
    
    ## Why
    The matrix-vector multiplication protocol needs to generate **uniform** random noise to hide the data passed around. Because the input data comes as `double` type, which is essentially unbounded; To generate **uniform** noise we have to first map it to a bounded range.
    
    ## How
    The arithmetic of `unsigned` type already forms an additive group: all results are automatically modulo `1 << width`. So we will just map the input `double` to an `unsigned` value.
    * Template parameters:
      * `FixedPointType`: The target `unsigned` type that will act as the integer group. Can be `uint16_t` to `uint64_t`.
      * `divisor`: Controls how much information are preserved after mapping. An input number will be mapped to the group as **the integral part** of `input * divisor`. One can view `log_10(divisor)` as the number of digits that will be preserved.
    
    ## Precision loss
    Let `output = mapToUnsignedDouble(mapToFixedPointType(input))`.
    
    Mapping a `double` to the group and then back can cause precision loss:
      * If `input < 0`: Since `unsigned` type only holds non-negative value, a negative `input` will be lost completely. To get correct result, use `mapToSignedDouble` instead.
      * If `input * divisor > (1 << width)`: The group size is too small to hold the desired number of digits. `input` will be lost completely.
      * Otherwise:
         (1). When `input` >= 1.0, the **relative** error will be less than `1 / divisor`.
         (2). When `input` < 1.0, the **absolute** error will be less than `1 / divisor`.
         (3). The error can accumulate when you map many `double` values to `FixedPointType`, sum them up, and then map the sum back to `double`. Users should be aware of this.
    
    Reviewed By: RuiyuZhu
    
    Differential Revision: D39062241
    
    fbshipit-source-id: d6c19175231b84b050c707a38b6b3e7e0179a96f

--- fbpcf/mpc_std_lib/walr_multiplication/util/NumberMapper.h
+++ fbpcf/mpc_std_lib/walr_multiplication/util/NumberMapper.h
@@ -7,10 +7,11 @@
 
 #pragma once
 
-#include <limits.h>
-#include <sys/types.h>
 #include <cstdint>
+#include <limits>
 #include <stdexcept>
+#include <vector>
+#include "folly/logging/xlog.h"
 
 namespace fbpcf::mpc_std_lib::walr::util {
 
@@ -21,6 +22,20 @@ class NumberMapper {
           std::is_unsigned_v<FixedPointType>,
       "Currently only support FixedPointType being some uint type with width no more than 64.");
 
+  using SignedFixedPointType = std::conditional_t<
+      std::is_same<FixedPointType, std::uint8_t>::value,
+      int8_t,
+      std::conditional_t<
+          std::is_same<FixedPointType, std::uint16_t>::value,
+          int16_t,
+          std::conditional_t<
+              std::is_same<FixedPointType, std::uint32_t>::value,
+              int32_t,
+              std::conditional_t<
+                  std::is_same<FixedPointType, std::uint64_t>::value,
+                  int64_t,
+                  int64_t>>>>;
+
  public:
   explicit NumberMapper(uint64_t divisor) : divisor_(divisor) {
     if (divisor > std::numeric_limits<FixedPointType>::max()) {
@@ -28,23 +43,79 @@ class NumberMapper {
           "The divisor's value should not exceed the max value a FixedPointType can represent.");
     }
   }
+  static constexpr uintmax_t groupSizeInt =
+      std::numeric_limits<FixedPointType>::max() + 1;
 
-  // Methods supporting converting from/to FixedPointType
-  inline FixedPointType mapToFixedPointType(double input) const;
+  static constexpr double groupSizeDouble = double(groupSizeInt);
+
+  /*
+   * Methods supporting converting from/to FixedPointType.
+   * Given an nonnegative double input, mapToDouble(mapToFixedPointType(input))
+   * preserves the input (up to precision loss).
+   * (1) When abs(input) >= 1.0, the RELATIVE precision loss will be
+   * less than abs(input) / divisor.
+   * (2) When abs(input) < 1.0, the ABSOLUTE precision loss will be
+   * less than 1 / divisor.
+   */
+  inline FixedPointType mapToFixedPointType(double input) const {
+    double product = input * divisor_;
+    if (std::abs(product) > groupSizeDouble) {
+      XLOG_EVERY_MS(WARN, 500) << "Magnitude of input number " << input
+                               << " too large. Conversion exceeds group size."
+                               << " May incur unwanted precision loss.";
+    }
+    uintmax_t rst = static_cast<uintmax_t>(static_cast<intmax_t>(product));
+    return static_cast<FixedPointType>(rst);
+  }
 
   std::vector<FixedPointType> mapToFixedPointType(
-      const std::vector<double>& input) const;
+      const std::vector<double>& input) const {
+    std::vector<FixedPointType> rst(input.size());
+    std::transform(input.cbegin(), input.cend(), rst.begin(), [this](double a) {
+      return mapToFixedPointType(a);
+    });
+    return rst;
+  }
 
-  inline double mapToDouble(FixedPointType input) const;
+  inline double mapToUnsignedDouble(FixedPointType input) const {
+    return input / static_cast<double>(divisor_);
+  }
 
-  std::vector<double> mapToDouble(
-      const std::vector<FixedPointType>& input) const;
+  std::vector<double> mapToUnsignedDouble(
+      const std::vector<FixedPointType>& input) const {
+    std::vector<double> rst(input.size());
+    std::transform(
+        input.cbegin(), input.cend(), rst.begin(), [this](FixedPointType a) {
+          return mapToUnsignedDouble(a);
+        });
+    return rst;
+  }
+
+  // The input will first be interpreted as a signed integer of the same width
+  inline double mapToSignedDouble(FixedPointType input) const {
+    return static_cast<SignedFixedPointType>(input) /
+        static_cast<double>(divisor_);
+  }
+
+  std::vector<double> mapToSignedDouble(
+      const std::vector<FixedPointType>& input) const {
+    std::vector<double> rst(input.size());
+    std::transform(
+        input.cbegin(), input.cend(), rst.begin(), [this](FixedPointType a) {
+          return mapToSignedDouble(a);
+        });
+    return rst;
+  }
 
   uint64_t getDivisor() const {
     return divisor_;
   }
 
   void setDivisor(uint64_t divisor) {
+    if (divisor > std::numeric_limits<FixedPointType>::max()) {
+      throw std::invalid_argument(
+          "The divisor's value should not exceed the max value a FixedPointType can represent.");
+    }
     divisor_ = divisor;
   }
 
---
+++ fbpcf/mpc_std_lib/walr_multiplication/util/test/NumberMapperTest.cpp
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <fbpcf/mpc_std_lib/walr_multiplication/util/NumberMapper.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <iterator>
+#include <limits>
+#include <random>
+#include <vector>
+
+namespace fbpcf::mpc_std_lib::walr::util {
+
+void testConversionEq(
+    const std::vector<double>& input,
+    const std::vector<double>& output,
+    uint64_t divisor) {
+  double tolerance = 1.0 / divisor;
+  ASSERT_EQ(input.size(), output.size());
+
+  // (1) When abs(input) >= 1.0, the RELATIVE precision loss should be
+  // less than abs(input) / divisor.
+  // (2) When abs(input) < 1.0, the ABSOLUTE precision loss should be
+  // less than 1 / divisor
+  for (size_t i = 0; i < output.size(); ++i) {
+    EXPECT_NEAR(
+        input[i],
+        output[i],
+        std::max({std::abs(input[i]), std::abs(output[i]), 1.0}) * tolerance);
+  }
+}
+
+TEST(numberMapperTest, testBasicConversion) {
+  constexpr uint64_t divisor = static_cast<uint64_t>(1e7);
+  auto mapper32 = NumberMapper<uint32_t>(divisor);
+  constexpr uint64_t groupSize =
+      (uint64_t)std::numeric_limits<uint32_t>::max() + 1;
+
+  // Basic conversion
+  // 1.0 / 7 = 0.1428571428571
+  EXPECT_EQ(mapper32.mapToFixedPointType(1.0 / 7), 1428571);
+  EXPECT_EQ(mapper32.mapToFixedPointType(7.0), 70000000);
+  EXPECT_EQ(mapper32.mapToFixedPointType(0.0), 0);
+
+  // Converting negative number
+  // On the integer group of size 2^32, -k is equivalent to 2^32 - k
+  EXPECT_EQ(mapper32.mapToFixedPointType(-1.0 / 7), groupSize - 1428571);
+
+  // Overflow
+  double input = ((uint32_t)1 << 31) + (1.0 / 7);
+  EXPECT_EQ(
+      mapper32.mapToFixedPointType(input),
+      (uint64_t)(input * divisor) % groupSize);
+}
+
+TEST(numberMapperTest, testUnsignedConversionPrecision) {
+  constexpr uint64_t divisor = static_cast<uint64_t>(1e9);
+  auto mapper64 = NumberMapper<uint64_t>(divisor);
+
+  std::random_device rd;
+  std::mt19937_64 e(rd());
+  std::uniform_real_distribution<double> dist(0, 10.1);
+  std::vector<double> input(99);
+  std::generate(input.begin(), input.end(), [&dist, &e]() { return dist(e); });
+  input.push_back(0.0);
+
+  auto output =
+      mapper64.mapToUnsignedDouble(mapper64.mapToFixedPointType(input));
+  testConversionEq(input, output, divisor);
+}
+
+TEST(numberMapperTest, testSignedConversionPrecision) {
+  constexpr uint64_t divisor = static_cast<uint64_t>(1e9);
+  auto mapper64 = NumberMapper<uint64_t>(divisor);
+
+  std::random_device rd;
+  std::mt19937_64 e(rd());
+  std::uniform_real_distribution<double> dist(-100.1, 100.1);
+  std::vector<double> input(99);
+  std::generate(input.begin(), input.end(), [&dist, &e]() { return dist(e); });
+  input.push_back(0.0);
+
+  auto output = mapper64.mapToSignedDouble(mapper64.mapToFixedPointType(input));
+  testConversionEq(input, output, divisor);
+}
+} // namespace fbpcf::mpc_std_lib::walr::util
