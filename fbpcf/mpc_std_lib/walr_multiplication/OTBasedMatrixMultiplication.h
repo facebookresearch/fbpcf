0fc61c1a40432ec5c854c18b1dd769d7b3f96fd2
Xiangyu Guo <xyguo@fb.com>
2022-09-06 09:27:34 -0700

    Prepare the interface for the customized matrix-vector multiplication protocol (#380)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/380
    
    This diff prepares the class interface for the customized matrix-vector multiplication protocol. The actual implementations will be given in subsequent diffs.
    
    ## Content
    
    * `OTBasedMatrixMultiplication.h`: Class definition for the customized protocol.
    * `OTBasedMatrixMultiplicationFactory.h`: Factory class definition for the above.
    * `NumberMapper`: Provide functionality for mapping a double number to a integer group (essentially a fixed-width unsigned number) and back.
        * This is a temporary solution that will be used specifically for the `OTBasedMatrixMultiplication` class.
    * `COTWithRandomMessage`: class definition for the Correlated Oblivious Transfer with Random Message.
        * This will be a temporary solution used specifically for the `OTBasedMatrixMultiplication` class. Later we shall implement a more general one in `fbpcf/engine/tuple_generator/oblivious_transfer`.
    * `COTWithRandomMessageFactory`: Factory class for the above.
    
    Reviewed By: RuiyuZhu
    
    Differential Revision: D38959811
    
    fbshipit-source-id: 24a1d73e33081fe060348a339d37f4c7a6fc7bd0

---
+++ fbpcf/mpc_std_lib/walr_multiplication/OTBasedMatrixMultiplication.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <emmintrin.h>
+#include <fbpcf/engine/tuple_generator/oblivious_transfer/IRandomCorrelatedObliviousTransferFactory.h>
+#include <fbpcf/engine/util/util.h>
+#include <algorithm>
+#include <cstdint>
+#include <stdexcept>
+#include <type_traits>
+#include "fbpcf/engine/communication/IPartyCommunicationAgent.h"
+#include "fbpcf/engine/communication/IPartyCommunicationAgentFactory.h"
+#include "fbpcf/engine/util/IPrgFactory.h"
+#include "fbpcf/mpc_std_lib/walr_multiplication/IWalrMatrixMultiplication.h"
+#include "fbpcf/mpc_std_lib/walr_multiplication/util/COTWithRandomMessage.h"
+#include "fbpcf/mpc_std_lib/walr_multiplication/util/NumberMapper.h"
+
+namespace fbpcf::mpc_std_lib::walr {
+
+template <int schedulerId, typename FixedPointType>
+class OTBasedMatrixMultiplication final
+    : public IWalrMatrixMultiplication<schedulerId> {
+ public:
+  explicit OTBasedMatrixMultiplication(
+      int myId,
+      int partnerId,
+      bool isFeatureOwner,
+      uint64_t divisor, // The precision loss will be roughly 1 / divisor
+      std::unique_ptr<engine::communication::IPartyCommunicationAgent> agent,
+      std::unique_ptr<engine::util::IPrgFactory> prgFactory,
+      std::unique_ptr<util::COTWithRandomMessage> cotWRM)
+      : myId_(myId),
+        partnerId_(partnerId),
+        isFeatureOwner_(isFeatureOwner),
+        numberMapper_(util::NumberMapper<FixedPointType>(divisor)),
+        agent_(std::move(agent)),
+        prgFactory_(std::move(prgFactory)),
+        cotWRM_(std::move(cotWRM)) {}
+
+  void setDivisor(uint64_t divisor) {
+    numberMapper_.setDivisor(divisor);
+  }
+
+  /**
+   * @inherit doc
+   */
+  std::vector<double> matrixVectorMultiplication(
+      const std::vector<std::vector<double>>& features,
+      const frontend::Bit<true, schedulerId, true>& labels) const;
+
+  /**
+   * @inherit doc
+   */
+  void matrixVectorMultiplication(
+      const frontend::Bit<true, schedulerId, true>& labels,
+      const std::vector<double>& dpNoise) const;
+
+ private:
+  int myId_;
+  int partnerId_;
+  bool isFeatureOwner_;
+  util::NumberMapper<FixedPointType> numberMapper_;
+  std::unique_ptr<engine::communication::IPartyCommunicationAgent> agent_;
+  std::unique_ptr<engine::util::IPrgFactory> prgFactory_;
+  std::unique_ptr<util::COTWithRandomMessage> cotWRM_;
+};
+} // namespace fbpcf::mpc_std_lib::walr
+
+#include "fbpcf/mpc_std_lib/walr_multiplication/OTBasedMatrixMultiplication_impl.h"
---
+++ fbpcf/mpc_std_lib/walr_multiplication/util/COTWithRandomMessage.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <emmintrin.h>
+#include <fbpcf/engine/tuple_generator/oblivious_transfer/IRandomCorrelatedObliviousTransfer.h>
+#include "fbpcf/engine/communication/IPartyCommunicationAgent.h"
+#include "fbpcf/engine/util/util.h"
+
+namespace fbpcf::mpc_std_lib::walr::util {
+
+class COTWithRandomMessage {
+ public:
+  // Sender constructor
+  COTWithRandomMessage(
+      __m128i delta,
+      std::unique_ptr<engine::communication::IPartyCommunicationAgent> agent,
+      std::unique_ptr<engine::tuple_generator::oblivious_transfer::
+                          IRandomCorrelatedObliviousTransfer> rcot)
+      : delta_(delta),
+        role_(engine::util::Role::sender),
+        agent_(std::move(agent)),
+        rcot_(std::move(rcot)) {}
+
+  // Receiver constructor
+  COTWithRandomMessage(
+      std::unique_ptr<engine::communication::IPartyCommunicationAgent> agent,
+      std::unique_ptr<engine::tuple_generator::oblivious_transfer::
+                          IRandomCorrelatedObliviousTransfer> rcot)
+      : role_(engine::util::Role::receiver),
+        agent_(std::move(agent)),
+        rcot_(std::move(rcot)) {}
+
+  /**
+   * run a number of COTwRM as the sender.
+   * @param size number of COTwR to run;
+   * @return a pair of 0-message vector and 1-message vector
+   */
+  std::pair<std::vector<__m128i>, std::vector<__m128i>> send(size_t size);
+
+  /**
+   * run a number of COTwRM as the receiver.
+   * @param choice the choice bit vector, its length indicates number of
+   * COTwRMs to run.
+   * @return chosen-message vector
+   */
+  std::vector<__m128i> receive(const std::vector<bool>& choice);
+
+  std::unique_ptr<engine::communication::IPartyCommunicationAgent>
+  extractCommunicationAgent() {
+    return std::move(agent_);
+  }
+
+  /**
+   * Get the total amount of traffic transmitted.
+   * @return a pair of (sent, received) data in bytes.
+   */
+  std::pair<uint64_t, uint64_t> getTrafficStatistics() const {
+    return agent_->getTrafficStatistics();
+  }
+
+ private:
+  __m128i delta_;
+  engine::util::Role role_;
+  std::unique_ptr<engine::communication::IPartyCommunicationAgent> agent_;
+  std::unique_ptr<engine::tuple_generator::oblivious_transfer::
+                      IRandomCorrelatedObliviousTransfer>
+      rcot_;
+};
+
+} // namespace fbpcf::mpc_std_lib::walr::util
