9554f404d423c9955c45a0557f01d30b30cdd4bf
Xiangyu Guo <xyguo@fb.com>
2022-09-06 09:27:34 -0700

    Add a matrix-vector multiplication API with a dummy implementation. (#350)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/350
    
    This diff creates the API for matrix multiplication in the WALR project. Currently the main goal is to provide functionality to multiply a feature matrix (owned by Meta) with a boolean label vector (owned by our partner). The result is only revealed to Meta.
    
    ### Content
    * API for classes that provides the matrix-vector multiplication functionality: `IWalrMatrixMultiplication`
    * A dummy (insecure) implementation that conduct the multiplication but leaks information: `DummyMatrixMultiplication`.
      * The two parties first reveals the labels to Meta (the feature owner side), then send the DP noise vector to Meta; Meta will multiply the feature matrix with the revealed label vector, then add the received DP noise to the result.
      * Communication cost of the dummy multiplier: `O(M+N)` where `N` is the size of the label vector and `M` is the size of the DP noise vector.
    * Unittest for the dummy multiplier.
    
    Reviewed By: chualynn
    
    Differential Revision: D38760026
    
    fbshipit-source-id: c8ad17d8ce632a18d0d2605f9a8a9d6ece84124f

---
+++ fbpcf/mpc_std_lib/walr_multiplication/DummyMatrixMultiplicationFactory.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "fbpcf/engine/communication/IPartyCommunicationAgentFactory.h"
+#include "fbpcf/mpc_std_lib/walr_multiplication/DummyMatrixMultiplication.h"
+#include "fbpcf/mpc_std_lib/walr_multiplication/IWalrMatrixMultiplication.h"
+#include "fbpcf/mpc_std_lib/walr_multiplication/IWalrMatrixMultiplicationFactory.h"
+
+namespace fbpcf::mpc_std_lib::walr::insecure {
+
+template <int schedulerId>
+class DummyMatrixMultiplicationFactory final
+    : public IWalrMatrixMultiplicationFactory<schedulerId> {
+ public:
+  explicit DummyMatrixMultiplicationFactory(
+      int myId,
+      int partnerId,
+      engine::communication::IPartyCommunicationAgentFactory& agentFactory)
+      : myId_(myId), partnerId_(partnerId), agentFactory_(agentFactory) {}
+
+  std::unique_ptr<IWalrMatrixMultiplication<schedulerId>> create() override {
+    return std::make_unique<DummyMatrixMultiplication<schedulerId>>(
+        myId_,
+        partnerId_,
+        agentFactory_.create(
+            partnerId_,
+            "walr_matrix_multiplication_traffic_to_party " +
+                std::to_string(partnerId_)));
+  }
+
+ private:
+  int myId_;
+  int partnerId_;
+  engine::communication::IPartyCommunicationAgentFactory& agentFactory_;
+};
+
+} // namespace fbpcf::mpc_std_lib::walr::insecure
