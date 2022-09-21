5478f66871132c41c29496b200148ff7c64ed2ad
Aditya Shastri <adshastri@fb.com>
2022-09-21 11:42:26 -0700

    Add option to specify root CA cert path (#1607)
    Summary:
    X-link: https://github.com/facebookresearch/fbpcs/pull/1607
    
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/399
    
    Reviewed By: danbunnell, RuiyuZhu
    
    Differential Revision: D39629266
    
    fbshipit-source-id: 957f1c28615c0295ffb8c2f1f5a12413e2273dab

--- fbpcf/engine/communication/test/TlsCommunicationUtils.h
+++ fbpcf/engine/communication/test/TlsCommunicationUtils.h
@@ -33,50 +33,101 @@ inline std::string setUpTlsFiles() {
 
   mkdir(dirToCreate.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
-  EVP_PKEY* pkey;
-  pkey = EVP_PKEY_new();
-
-  RSA* rsa;
-  rsa = RSA_generate_key(
+  /*
+   * Create Root CA cert
+   */
+  RSA* rsa_for_ca;
+  rsa_for_ca = RSA_generate_key(
       4096, /* number of bits for the key */
       RSA_F4, /* exponent */
       nullptr, /* callback */
       nullptr /* callback argument */
   );
 
-  EVP_PKEY_assign_RSA(pkey, rsa);
+  EVP_PKEY* pkey_for_ca;
+  pkey_for_ca = EVP_PKEY_new();
 
-  X509* x509;
-  x509 = X509_new();
+  EVP_PKEY_assign_RSA(pkey_for_ca, rsa_for_ca);
 
-  // expiry date
-  X509_gmtime_adj(X509_get_notBefore(x509), 0);
-  X509_gmtime_adj(X509_get_notAfter(x509), 31536000L); // 1 year in seconds
+  X509* x509_for_ca;
+  x509_for_ca = X509_new();
 
-  X509_set_pubkey(x509, pkey);
+  X509_gmtime_adj(X509_get_notBefore(x509_for_ca), 0);
+  X509_gmtime_adj(
+      X509_get_notAfter(x509_for_ca), 31536000L); // 1 year in seconds
 
-  X509_NAME* name;
-  name = X509_get_subject_name(x509);
+  X509_set_pubkey(x509_for_ca, pkey_for_ca);
+
+  X509_NAME* name_for_ca;
+  name_for_ca = X509_get_subject_name(x509_for_ca);
 
   // These are metadata about the host, like country,
   // company name, etc. They are required, but not important
   // because these are just self signed for testing purposes.
   // We can leave them as empty strings.
   X509_NAME_add_entry_by_txt(
-      name, "C", MBSTRING_ASC, (unsigned char*)"", -1, -1, 0);
+      name_for_ca, "C", MBSTRING_ASC, (unsigned char*)"", -1, -1, 0);
   X509_NAME_add_entry_by_txt(
-      name, "O", MBSTRING_ASC, (unsigned char*)"", -1, -1, 0);
+      name_for_ca, "O", MBSTRING_ASC, (unsigned char*)"root org", -1, -1, 0);
   X509_NAME_add_entry_by_txt(
-      name, "CN", MBSTRING_ASC, (unsigned char*)"", -1, -1, 0);
-  X509_set_issuer_name(x509, name);
-  X509_sign(x509, pkey, EVP_sha1());
+      name_for_ca, "CN", MBSTRING_ASC, (unsigned char*)"test ca", -1, -1, 0);
+  X509_set_issuer_name(x509_for_ca, name_for_ca);
+  X509_sign(x509_for_ca, pkey_for_ca, EVP_sha1());
+
+  /*
+   * Create server cert signed using root CA
+   */
+  RSA* rsa_for_server;
+  rsa_for_server = RSA_generate_key(
+      4096, /* number of bits for the key */
+      RSA_F4, /* exponent */
+      nullptr, /* callback */
+      nullptr /* callback argument */
+  );
+
+  EVP_PKEY* pkey_for_server;
+  pkey_for_server = EVP_PKEY_new();
+
+  EVP_PKEY_assign_RSA(pkey_for_server, rsa_for_server);
+
+  // write cert to file
+  FILE* cafile;
+  cafile = fopen((dirToCreate + "/ca_cert.pem").c_str(), "wb");
+  PEM_write_X509(cafile, x509_for_ca);
+  fclose(cafile);
+
+  X509* x509_for_server;
+  x509_for_server = X509_new();
+
+  // expiry date
+  X509_gmtime_adj(X509_get_notBefore(x509_for_server), 0);
+  X509_gmtime_adj(
+      X509_get_notAfter(x509_for_server), 31536000L); // 1 year in seconds
+
+  X509_set_pubkey(x509_for_server, pkey_for_server);
+
+  X509_NAME* name_for_server;
+  name_for_server = X509_get_subject_name(x509_for_server);
+
+  // These are metadata about the host, like country,
+  // company name, etc. They are required, but not important
+  // because these are just self signed for testing purposes.
+  // We can leave them as empty strings.
+  X509_NAME_add_entry_by_txt(
+      name_for_server, "C", MBSTRING_ASC, (unsigned char*)"", -1, -1, 0);
+  X509_NAME_add_entry_by_txt(
+      name_for_server, "O", MBSTRING_ASC, (unsigned char*)"", -1, -1, 0);
+  X509_NAME_add_entry_by_txt(
+      name_for_server, "CN", MBSTRING_ASC, (unsigned char*)"", -1, -1, 0);
+  X509_set_issuer_name(x509_for_server, name_for_ca);
+  X509_sign(x509_for_server, pkey_for_ca, EVP_sha1());
 
   // write private key to file
   FILE* keyfile;
   keyfile = fopen((dirToCreate + "/key.pem").c_str(), "wb");
   PEM_write_PrivateKey(
       keyfile,
-      pkey,
+      pkey_for_server,
       EVP_des_ede3_cbc(), /* cipher */
       (unsigned char*)"test_passphrase", /* for tests only */
       15, /* length of the passphrase string */
@@ -88,7 +139,7 @@ inline std::string setUpTlsFiles() {
   // write cert to file
   FILE* certfile;
   certfile = fopen((dirToCreate + "/cert.pem").c_str(), "wb");
-  PEM_write_X509(certfile, x509);
+  PEM_write_X509(certfile, x509_for_server);
   fclose(certfile);
 
   // write passphrase to file
