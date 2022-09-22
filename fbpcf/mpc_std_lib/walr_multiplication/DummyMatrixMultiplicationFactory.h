ca35e8db4c8381c4c550f2fc1f36bc0fb8732f1f
Xiangyu Guo <xyguo@fb.com>
2022-09-22 15:15:38 -0700

    Pass metric collectors to matrix multiplication factories
    Summary:
    ## Why
    To support logging using metric collector for the matrix multiplication classes.
    
    ## What
    We add new constructors for `DummyMatrixMultiplicationFactory` and `OTBasedMatrixMultiplicationFactory`, such that they can accept a metric collector argument.
    
    Reviewed By: adshastri
    
    Differential Revision: D39712334
    
    fbshipit-source-id: 2aba3533f2b3808d7ec17534dfcb09dca77ecb45

--- fbpcf/mpc_std_lib/walr_multiplication/DummyMatrixMultiplicationFactory.h
+++ fbpcf/mpc_std_lib/walr_multiplication/DummyMatrixMultiplicationFactory.h
@@ -17,15 +17,40 @@ namespace fbpcf::mpc_std_lib::walr::insecure {
 template <int schedulerId>
 class DummyMatrixMultiplicationFactory final
     : public IWalrMatrixMultiplicationFactory<schedulerId> {
+  using IWalrMatrixMultiplicationFactory<schedulerId>::metricCollector_;
+
  public:
+  const std::string metricRecorderNamePrefix = "dummy_matrix_multiplication";
+
+  // The following constructor will be deprecated once we updated all APP codes
   explicit DummyMatrixMultiplicationFactory(
       int myId,
       int partnerId,
       engine::communication::IPartyCommunicationAgentFactory& agentFactory)
-      : myId_(myId), partnerId_(partnerId), agentFactory_(agentFactory) {}
+      : IWalrMatrixMultiplicationFactory<schedulerId>(nullptr),
+        myId_(myId),
+        partnerId_(partnerId),
+        agentFactory_(agentFactory) {}
+
+  explicit DummyMatrixMultiplicationFactory(
+      int myId,
+      int partnerId,
+      engine::communication::IPartyCommunicationAgentFactory& agentFactory,
+      std::shared_ptr<fbpcf::util::MetricCollector> metricCollector)
+      : IWalrMatrixMultiplicationFactory<schedulerId>(metricCollector),
+        myId_(myId),
+        partnerId_(partnerId),
+        agentFactory_(agentFactory) {}
 
   std::unique_ptr<IWalrMatrixMultiplication<schedulerId>> create() override {
     auto recorder = std::make_shared<WalrMatrixMultiplicationMetricRecorder>();
+
+    // For backward compatibility we currently allow a null metric collector.
+    // The condition will be removed later when we change all apps to use a
+    // metric collector.
+    if (metricCollector_ != nullptr) {
+      metricCollector_->addNewRecorder(metricRecorderNamePrefix, recorder);
+    }
     return std::make_unique<DummyMatrixMultiplication<schedulerId>>(
         myId_,
         partnerId_,
