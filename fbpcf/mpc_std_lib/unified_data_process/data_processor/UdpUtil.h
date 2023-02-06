cb73e5d64b59ef9bb5a80422875950748f98eb6f
Ruiyu Zhu <ruiyuzhu@meta.com>
2023-02-06 13:56:14 -0800

    Move some data processor code to a util class (#478)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/478
    
    We plan to have seperate UDP encryption and decryption steps in different stages. These util functions can be reused in that architecture.
    
    Reviewed By: robotal
    
    Differential Revision: D42900682
    
    fbshipit-source-id: bd68847bea6a7b603553be50a02007c013c34457

---
+++ fbpcf/mpc_std_lib/unified_data_process/data_processor/UdpUtil.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <emmintrin.h>
+#include <sys/types.h>
+#include "fbpcf/frontend/BitString.h"
+#include "fbpcf/primitive/mac/S2v.h"
+#include "fbpcf/primitive/mac/S2vFactory.h"
+
+namespace fbpcf::mpc_std_lib::unified_data_process::data_processor {
+
+class UdpUtil {
+  template <int schedulerId>
+  using SecBit = frontend::Bit<true, schedulerId, true>;
+
+  static const size_t kBlockSize = 16;
+
+ public:
+  static std::pair<std::vector<std::vector<uint8_t>>, std::vector<uint8_t>>
+  localEncryption(
+      const std::vector<std::vector<unsigned char>>& plaintextData,
+      __m128i prgKey,
+      uint64_t indexOffset = 0);
+
+  static std::vector<__m128i>
+  generateCounterBlocks(__m128i nonce, uint64_t startingIndex, size_t size);
+
+  template <int schedulerId>
+  static std::vector<SecBit<schedulerId>> privatelyShareByteStream(
+      const std::vector<std::vector<unsigned char>>& localData,
+      int inputPartyID);
+
+  template <int schedulerId>
+  static std::vector<SecBit<schedulerId>> privatelyShareM128iStream(
+      const std::vector<std::vector<__m128i>>& localDataM128i,
+      int inputPartyID);
+
+  template <int schedulerId>
+  static std::vector<SecBit<schedulerId>> privatelyShareExpandedKey(
+      const std::vector<__m128i>& localKeyM128i,
+      size_t batchSize,
+      int inputPartyID);
+};
+
+} // namespace fbpcf::mpc_std_lib::unified_data_process::data_processor
+
+#include "fbpcf/mpc_std_lib/unified_data_process/data_processor/UdpUtil_impl.h"
