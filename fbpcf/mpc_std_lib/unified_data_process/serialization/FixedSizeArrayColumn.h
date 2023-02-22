583d553de0a5b4b4fde3f443e067c58fa52094f5
Tal Davidi <tdavidi@fb.com>
2023-02-22 10:17:41 -0800

    Add padded vector column type
    Differential Revision: D43366171
    
    fbshipit-source-id: 3ca49c13489a99e25d4bb5c0d2b170e5d8382d26

---
+++ fbpcf/mpc_std_lib/unified_data_process/serialization/FixedSizeArrayColumn.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <memory>
+#include "fbpcf/mpc_std_lib/unified_data_process/serialization/IColumnDefinition.h"
+
+namespace fbpcf::mpc_std_lib::unified_data_process::serialization {
+
+template <int schedulerId, typename InnerType>
+class FixedSizeArrayColumn : public IColumnDefinition<schedulerId> {
+ public:
+  FixedSizeArrayColumn(
+      std::string columnName,
+      std::unique_ptr<IColumnDefinition<schedulerId>> innerType,
+      size_t length)
+      : columnName_{columnName},
+        innerType_{std::move(innerType)},
+        length_{length} {}
+
+  std::string getColumnName() const override {
+    return columnName_;
+  }
+
+  size_t getColumnSizeBytes() const override {
+    return length_ * innerType_->getColumnSizeBytes();
+  }
+
+  size_t getLength() const {
+    return length_;
+  }
+
+  void serializeColumnAsPlaintextBytes(
+      const void* inputData,
+      unsigned char* buf) const override {
+    size_t innerTypeSize = innerType_->getColumnSizeBytes();
+    for (int i = 0; i < length_; i++) {
+      size_t offset = innerTypeSize * i;
+      innerType_->serializeColumnAsPlaintextBytes(
+          (const void*)(((const unsigned char*)inputData) + offset),
+          buf + offset);
+    }
+  }
+
+  typename IColumnDefinition<schedulerId>::DeserializeType
+  deserializeSharesToMPCType(
+      const std::vector<std::vector<unsigned char>>& serializedSecretShares,
+      size_t byteOffset) const override {
+    auto rst = std::vector<InnerType>(0);
+
+    size_t innerTypeSize = innerType_->getColumnSizeBytes();
+
+    for (int i = 0; i < length_; i++) {
+      size_t offset = byteOffset + innerTypeSize * i;
+      typename IColumnDefinition<schedulerId>::DeserializeType innerVal =
+          innerType_->deserializeSharesToMPCType(
+              serializedSecretShares, offset);
+      rst.push_back(std::get<InnerType>(innerVal));
+    }
+
+    return rst;
+  }
+
+ private:
+  std::string columnName_;
+  std::unique_ptr<IColumnDefinition<schedulerId>> innerType_;
+  size_t length_;
+};
+
+} // namespace fbpcf::mpc_std_lib::unified_data_process::serialization
--- fbpcf/mpc_std_lib/unified_data_process/serialization/test/SerializationTest.cpp
+++ fbpcf/mpc_std_lib/unified_data_process/serialization/test/SerializationTest.cpp
@@ -46,6 +46,35 @@ static std::vector<int32_t> deserializeAndRevealInt32(
   return rst;
 }
 
+template <int schedulerId>
+static std::vector<std::vector<int32_t>> deserializeAndRevealInt32Vector(
+    fbpcf::scheduler::ISchedulerFactory<true>& schedulerFactory,
+    const std::vector<std::vector<unsigned char>>& serializedSecretShares,
+    IColumnDefinition<schedulerId>& serializer) {
+  auto scheduler = schedulerFactory.create();
+
+  fbpcf::scheduler::SchedulerKeeper<schedulerId>::setScheduler(
+      std::move(scheduler));
+
+  typename IColumnDefinition<schedulerId>::DeserializeType mpcValue =
+      serializer.deserializeSharesToMPCType(serializedSecretShares, 0);
+  std::vector<typename frontend::MPCTypes<schedulerId>::Sec32Int> visitedVal =
+      std::get<std::vector<typename frontend::MPCTypes<schedulerId>::Sec32Int>>(
+          mpcValue);
+
+  std::vector<std::vector<int32_t>> rst(
+      visitedVal.size(), std::vector<int32_t>(visitedVal[0].getBatchSize()));
+
+  for (int i = 0; i < visitedVal.size(); i++) {
+    std::vector<int64_t> rst64 = visitedVal[i].openToParty(0).getValue();
+    std::transform(rst64.begin(), rst64.end(), rst[i].begin(), [](int64_t val) {
+      return val;
+    });
+  }
+
+  return rst;
+}
+
 TEST(SerializationTest, IntegerColumnTest) {
   auto factories = fbpcf::engine::communication::getInMemoryAgentFactory(2);
 
@@ -101,6 +130,81 @@ TEST(SerializationTest, IntegerColumnTest) {
   auto rst = future0.get();
   future1.get();
   testVectorEq(vals, rst);
+}
+
+TEST(SerializationTest, ArrayColumnTest) {
+  auto factories = fbpcf::engine::communication::getInMemoryAgentFactory(2);
+
+  auto schedulerFactory0 =
+      fbpcf::scheduler::NetworkPlaintextSchedulerFactory<true>(
+          0, *factories[0]);
+
+  auto schedulerFactory1 =
+      fbpcf::scheduler::NetworkPlaintextSchedulerFactory<true>(
+          1, *factories[1]);
+
+  const size_t batchSize = 100;
+  const size_t paddingSize = 4;
+
+  std::random_device rd;
+  std::mt19937_64 e(rd());
+  std::uniform_int_distribution<int32_t> dist(
+      std::numeric_limits<int32_t>().min(),
+      std::numeric_limits<int32_t>().max());
+
+  FixedSizeArrayColumn<0, frontend::MPCTypes<0>::Sec32Int> serializer0(
+      "testColumnName",
+      std::make_unique<IntegerColumn<0, true, 32>>("testColumnName"),
+      paddingSize);
+  FixedSizeArrayColumn<1, frontend::MPCTypes<1>::Sec32Int> serializer1(
+      "testColumnName",
+      std::make_unique<IntegerColumn<1, true, 32>>("testColumnName"),
+      paddingSize);
+  EXPECT_EQ(serializer0.getColumnSizeBytes(), paddingSize * 4);
+
+  std::vector<std::vector<uint8_t>> bufs(
+      batchSize, std::vector<uint8_t>(serializer0.getColumnSizeBytes()));
+
+  std::vector<std::vector<int32_t>> vals(
+      paddingSize, std::vector<int32_t>(batchSize));
+
+  for (int i = 0; i < batchSize; i++) {
+    std::vector<int32_t> val(paddingSize);
+    for (int j = 0; j < paddingSize; j++) {
+      val[j] = dist(e);
+      vals[j][i] = val[j];
+    }
+
+    serializer0.serializeColumnAsPlaintextBytes(val.data(), bufs[i].data());
+
+    for (int j = 0; j < paddingSize; j++) {
+      EXPECT_EQ(bufs[i][0 + j * sizeof(int32_t)], val[j] & 255);
+      EXPECT_EQ(bufs[i][1 + j * sizeof(int32_t)], (val[j] >> 8) & 255);
+      EXPECT_EQ(bufs[i][2 + j * sizeof(int32_t)], (val[j] >> 16) & 255);
+      EXPECT_EQ(bufs[i][3 + j * sizeof(int32_t)], (val[j] >> 24) & 255);
+    }
+  }
+
+  auto future0 = std::async([&schedulerFactory0, &bufs, &serializer0]() {
+    return deserializeAndRevealInt32Vector<0>(
+        schedulerFactory0, bufs, serializer0);
+  });
+
+  auto future1 = std::async([&schedulerFactory1, &serializer1]() {
+    return deserializeAndRevealInt32Vector<1>(
+        schedulerFactory1,
+        std::vector<std::vector<uint8_t>>(
+            batchSize, std::vector<uint8_t>(serializer1.getColumnSizeBytes())),
+        serializer1);
+  });
+
+  auto rst = future0.get();
+  future1.get();
+
+  EXPECT_EQ(rst.size(), paddingSize);
+  for (int j = 0; j < paddingSize; j++) {
+    testVectorEq(vals[j], rst[j]);
+  }
 }
 
 } // namespace fbpcf::mpc_std_lib::unified_data_process::serialization
