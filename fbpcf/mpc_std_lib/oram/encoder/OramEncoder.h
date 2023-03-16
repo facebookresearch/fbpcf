327bc3e9a5a5cd56ab237e84c41998f4f999c791
Tal Davidi <tdavidi@fb.com>
2023-03-16 13:23:53 -0700

    Implement ORAM encoding without filters
    Differential Revision: D44040848
    
    fbshipit-source-id: 67eaf2ed651ef006205986737b7c075c36b20f9b

--- fbpcf/mpc_std_lib/oram/encoder/OramEncoder.h
+++ fbpcf/mpc_std_lib/oram/encoder/OramEncoder.h
@@ -8,20 +8,30 @@
 #pragma once
 
 #include <stdexcept>
+#include <unordered_map>
+#include "folly/String.h"
+
 #include "fbpcf/mpc_std_lib/oram/encoder/IOramEncoder.h"
 
 namespace fbpcf::mpc_std_lib::oram {
 
 class OramEncoder : public IOramEncoder {
  public:
-  explicit OramEncoder() {}
+  explicit OramEncoder() : currentIndex_(1), breakdownMapping_() {}
 
   std::vector<uint32_t> generateORAMIndexes(
-      const std::vector<std::vector<uint32_t>>& /* tuples */) override;
+      const std::vector<std::vector<uint32_t>>& tuples) override;
 
   std::unique_ptr<OramMappingConfig> exportMappingConfig() const override;
 
  private:
+  uint32_t currentIndex_;
+  std::unordered_map<std::string, uint32_t> breakdownMapping_;
+
+  std::string convertBreakdownsToKey(
+      const std::vector<uint32_t>& breakdownValues) const {
+    return folly::join(",", breakdownValues);
+  }
 };
 
 } // namespace fbpcf::mpc_std_lib::oram
