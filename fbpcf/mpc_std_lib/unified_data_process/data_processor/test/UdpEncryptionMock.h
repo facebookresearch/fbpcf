94b3262c3fc21ebf6f4a11840a67bb6ee83a41c8
Ruiyu Zhu <ruiyuzhu@meta.com>
2023-03-22 10:17:31 -0700

    Move mock back to fbpcf (#2224)
    Summary:
    X-link: https://github.com/facebookresearch/fbpcs/pull/2224
    
    Pull Request resolved: https://github.com/facebookresearch/fbpcf/pull/515
    
    This was a less optimal engineering decision. It becomes quite challenging if we want to modify the interface while the mock lives in a different repo.
    
    Reviewed By: xyguo
    
    Differential Revision: D44261909
    
    fbshipit-source-id: dc13eb0d14943d045c8a76d84e82671c42e8a2d1

---
+++ fbpcf/mpc_std_lib/unified_data_process/data_processor/test/UdpEncryptionMock.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "fbpcf/mpc_std_lib/unified_data_process/data_processor/IUdpEncryption.h"
+
+namespace fbpcf::mpc_std_lib::unified_data_process::data_processor {
+
+using namespace ::testing;
+
+class UdpEncryptionMock final : public IUdpEncryption {
+ public:
+  MOCK_METHOD(void, prepareToProcessMyData, (size_t));
+
+  MOCK_METHOD(
+      void,
+      processMyData,
+      (const std::vector<std::vector<unsigned char>>&));
+
+  MOCK_METHOD(std::vector<__m128i>, getExpandedKey, ());
+
+  MOCK_METHOD(
+      void,
+      prepareToProcessPeerData,
+      (size_t, const std::vector<int32_t>&));
+
+  MOCK_METHOD(void, processPeerData, (size_t));
+
+  MOCK_METHOD(EncryptionResults, getProcessedData, ());
+};
+
+} // namespace fbpcf::mpc_std_lib::unified_data_process::data_processor
